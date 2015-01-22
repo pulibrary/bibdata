5511f78ddd7b4ad9f4def68381ee701daec039f2
Jon Stroop <jpstroop@gmail.com>
2015-01-22 16:44:15 -0500

    set up rspec

---
+++ voyager_helpers/spec/unit/voyager_helpers/sync_fu_spec.rb
@@ -0,0 +1,15 @@
+describe VoyagerHelpers::SyncFu do
+
+  let(:fixtures_dir) { File.join(File.dirname(__FILE__), '../../fixtures') }
+  let(:earlier_file) { File.join(fixtures_dir, 'earlier_id_dump.txt') }
+  let(:later_file) { File.join(fixtures_dir, 'later_id_dump.txt') }
+  let(:subject) { described_class }
+  describe '#compare_id_dumps' do
+
+    it 'sorts the entries as expected' do
+
+    end
+  end
+end
+
+
