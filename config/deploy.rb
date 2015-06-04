f3b85893c5257fa308aa5eefbdb9aff7c5c34a01
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2015-06-04 15:35:33 -0400

    cap version 3.4.0 for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.3.5'
+lock '3.4.0'
 
 set :application, 'marc_liberation'
 set :repo_url, "git@github.com:pulibrary/#{fetch(:application)}.git"
