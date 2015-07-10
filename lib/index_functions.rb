41015bec760355d0f5caf3b56e347b50d334561e
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2015-07-10 15:44:01 -0400

    cjk fields, rel_name trim_punctuation bug fix

--- lib/index_functions.rb
+++ lib/index_functions.rb
@@ -13,28 +13,12 @@ module IndexFunctions
     # updates
     dump['files']['updated_records'].each_with_index do |update, i|
       File.write("/tmp/update_#{i}.gz", Faraday.get(update['dump_file']).body)
-      Zlib::GzipReader.open("/tmp/update_#{i}.gz") do |gz|
-        File.open("/tmp/update_#{i}.xml", 'w') do |fp|
-          while chunk = gz.read(16 * 1024) do
-            fp.write chunk
-          end
-        end
-        gz.close
-      end
       file_paths << "/tmp/update_#{i}"
     end
 
     # new records
     dump['files']['new_records'].each_with_index do |new_records, i|
       File.write("/tmp/new_#{i}.gz", Faraday.get(new_records['dump_file']).body)
-      Zlib::GzipReader.open("/tmp/new_#{i}.gz") do |gz|
-        File.open("/tmp/new_#{i}.xml", "w") do |fp|
-          while chunk = gz.read(16 * 1024) do
-            fp.write chunk
-          end
-        end
-        gz.close
-      end 
       file_paths << "/tmp/new_#{i}"
     end  
 
@@ -47,20 +31,22 @@ module IndexFunctions
   def self.full_dump(event, solr_url)
     file_paths = []
     dump = JSON.parse(Faraday.get(event['dump_url']).body)
-
     dump['files']['bib_records'].each_with_index do |bib, i|
       File.write("/tmp/bib_#{i}.gz", Faraday.get(bib['dump_file']).body)
-      Zlib::GzipReader.open("/tmp/bib_#{i}.gz") do |gz|
-        File.open("/tmp/bib_#{i}.xml", 'w') do |fp|
-          while chunk = gz.read(16 * 1024) do
-            fp.write chunk
-          end
-        end
-        gz.close
-      end
       file_paths << "/tmp/bib_#{i}"
     end
     file_paths
   end
 
+
+  def self.unzip(marc_dump)
+    Zlib::GzipReader.open("#{marc_dump}.gz") do |gz|
+      File.open("#{marc_dump}.xml", 'w') do |fp|
+        while chunk = gz.read(16 * 1024) do
+          fp.write chunk
+        end
+      end
+      gz.close
+    end
+  end
 end
