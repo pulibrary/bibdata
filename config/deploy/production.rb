0102819716d61b83affd26afd13ef2b3f9e53bf3
Trey Pendragon <tpendragon@princeton.edu>
2017-01-04 08:31:17 -0800

    Deploy master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 set :stage, :production
 set :rails_env, 'production'
 server 'bibdata', user: 'deploy', roles: [:web, :app, :db]
-set :branch, ENV['BRANCH'] || 'development'
+set :branch, ENV['BRANCH'] || 'master'
 
 # Extended Server Syntax
 # ======================
