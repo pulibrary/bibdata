a46a5ec343bc42db527e951ec36c8fb33ba0c370
christinach <actspatial@gmail.com>
2018-10-02 09:09:30 -0400

    update rubocop_todo

--- marc_to_solr/spec/lib/uri_ark_spec.rb
+++ marc_to_solr/spec/lib/uri_ark_spec.rb
@@ -3,6 +3,7 @@ require_relative '../../lib/uri_ark'
 RSpec.describe URI::ARK do
   describe '#princeton_ark?' do
     let(:princeton_ark) { 'http://arks.princeton.edu/ark:/88435/9k41zd556' }
+
     let(:non_princeton_ark) { 'https://nls.ldls.org.uk/welcome.html?ark:/81055/vdc_100052020059.0x000001' }
     it 'Princeton arks return true' do
       expect(described_class.princeton_ark?(url: princeton_ark)).to eq true
