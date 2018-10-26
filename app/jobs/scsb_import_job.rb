64907e0ec9217a49b2fe6622800919d954e7073e
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2018-10-26 11:35:59 -0400

    update timestamp

--- app/jobs/scsb_import_job.rb
+++ app/jobs/scsb_import_job.rb
@@ -2,8 +2,8 @@ class ScsbImportJob < ActiveJob::Base
   include Scsb
   queue_as :default
 
-  def perform(dump_id)
+  def perform(dump_id, timestamp)
     dump = Dump.find(dump_id)
-    Scsb::PartnerUpdates.new(dump: dump).process_partner_files
+    Scsb::PartnerUpdates.new(dump: dump, timestamp: timestamp).process_partner_files
   end
 end
