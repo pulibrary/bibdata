68aa873184d82d52b37b0e89c0b7d250c40ef30a
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2020-03-17 18:31:54 -0400

    adds unstem_search fields for subjects, removes combined subject display field

--- marc_to_solr/lib/traject_config.rb
+++ marc_to_solr/lib/traject_config.rb
@@ -735,12 +735,6 @@ to_field 'cumulative_index_finding_aid_display', extract_marc('555|8*|3abcd')
 #    630 XX adfgklmnoprst{v--%}{x--%}{y--%}{z--%} S adfgklmnoprstvxyz
 #    650 XX abc{v--%}{x--%}{z--%}{y--%} S abcvxyz
 #    651 XX a{v--%}{x--%}{y--%}{z--%} S avxyz
-to_field 'subject_display' do |record, accumulator|
-  subjects = process_hierarchy(record, '600|*0|abcdfklmnopqrtvxyz:610|*0|abfklmnoprstvxyz:611|*0|abcdefgklnpqstvxyz:630|*0|adfgklmnoprstvxyz:650|*0|abcvxyz:651|*0|avxyz')
-  sk_subjects = process_hierarchy(record, '650|*7|abcvxyz', ['sk'])
-  accumulator.replace([subjects, sk_subjects].flatten)
-end
-
 to_field 'lc_subject_display' do |record, accumulator|
   subjects = process_hierarchy(record, '600|*0|abcdfklmnopqrtvxyz:610|*0|abfklmnoprstvxyz:611|*0|abcdefgklnpqstvxyz:630|*0|adfgklmnoprstvxyz:650|*0|abcvxyz:651|*0|avxyz')
   accumulator.replace(subjects)
@@ -750,6 +744,13 @@ to_field 'siku_subject_display' do |record, accumulator|
   genres = process_hierarchy(record, '650|*7|abcvxyz', ['sk'])
   accumulator.replace(genres)
 end
+
+# Adds lc and siku subject unstem_search fields
+each_record do |_record, context|
+  context.output_hash['subject_unstem_search'] = context.output_hash['lc_subject_display']
+  context.output_hash['siku_subject_unstem_search'] = context.output_hash['siku_subject_display']
+end
+
 
 # used for the browse lists and hierarchical subject/genre facet
 to_field 'subject_facet' do |record, accumulator|
