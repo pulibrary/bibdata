592be607364482fe49e51b1c61fd884e6253edee
christinach <actspatial@gmail.com>
2020-05-19 11:14:56 -0400

    Update spec
    Co-authored-by: Carolyn Cole <cac9@princeton.edu>

--- app/models/hathi/compact_full.rb
+++ app/models/hathi/compact_full.rb
@@ -2,17 +2,16 @@ require "csv"
 
 module Hathi
   class CompactFull
-    
-    @@hathi_dir = ENV['HATHI_FULL']
-    @@output_hathi_dir = ENV['OUTPUT_HATHI']
-    
+
     def self.get_full_hathi_file
-      Dir.glob("#{@@hathi_dir}/hathi_full*").sort_by { |filename| filename.to_date.strftime}.last
+      hathi_dir = ENV['HATHI_FULL']
+      Dir.glob("#{hathi_dir}/hathi_full*").sort_by { |filename| filename.to_date.strftime}.last
     end  
 
     def self.compact_full
-      CSV.open("#{@@output_hathi_dir}/compact_hathi_full.csv", "wb", col_sep: "\t") do |csv|
-        csv << ["url","oclc"]
+      output_hathi_dir = ENV['OUTPUT_HATHI']
+      CSV.open("#{output_hathi_dir}/compact_hathi_full.csv", "wb", col_sep: "\t") do |csv|
+        csv << ["identifier","oclc"]
         CSV.foreach(get_full_hathi_file, col_sep: "\t", headers: true) do |row|
           csv << [row[0],row[7]]
         end
--- spec/models/hathi/compact_full_spec.rb
+++ spec/models/hathi/compact_full_spec.rb
@@ -3,14 +3,25 @@ require 'rails_helper'
 RSpec.describe Hathi::CompactFull do
   describe '#compact_full' do
     
-    let(:hathi_directory) { File.expand_path('../../fixtures/', __FILE__)}
-    let(:hathi_output) { File.expand_path('../../../tmp', __FILE__)}
+    let(:hathi_directory) { File.expand_path('../../../fixtures/', __FILE__)}
+    let(:hathi_output) { File.expand_path('../../../../tmp', __FILE__)}
     let(:compact_file) {File.join(hathi_output, 'compact_hathi_full.csv')}
+    
     it 'compacts the full hathi file' do
       ENV['OUTPUT_HATHI'] = hathi_output
       ENV['HATHI_FULL'] = hathi_directory
-      
-      
+      output_file = File.open(compact_file)
+      described_class.compact_full
+      expect(output_file.read).to eq("identifier\toclc\n"\
+      	                             "mdp.39015066356547\t2779601\n"\
+      	                             "mdp.39015066356406\t2779601\n"\
+      	                             "mdp.39015066356695\t2779601\n"\
+      	                             "mdp.39015066356554\t2779601\n"\
+      	                             "uc1.$b759626\t2779601\n"\
+                                     "uc1.$b759627\t2779601\n"\
+      	                             "uc1.$b759628\t2779601\n"\
+      	                             "mdp.39015033913115\t23536349\n"\
+      	                             "mdp.39015061455294\t60561774\n") 
     end
   end    
 end
