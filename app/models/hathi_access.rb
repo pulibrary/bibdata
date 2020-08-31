0ab6b21703f69bacae0962be949eea2076ab440b
Anna Headley <anna.headley@gmail.com>
2020-08-31 10:32:50 -0400

    Add validation for status and origin
    refs #757

--- app/models/hathi_access.rb
+++ app/models/hathi_access.rb
@@ -1,2 +1,4 @@
-class HathiAccess < ApplicationRecord
+class HathiAccess < ActiveRecord::Base
+  validates :status, inclusion: { in: %w(ALLOW DENY) }
+  validates :origin, inclusion: { in: %w(CUL PUL) }
 end
