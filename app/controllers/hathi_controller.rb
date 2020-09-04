0b4c8686951a421d644060a30324a23c14dfede0
christinach <actspatial@gmail.com>
2020-09-04 09:29:56 -0400

    Remove again the old route method

--- app/controllers/hathi_controller.rb
+++ app/controllers/hathi_controller.rb
@@ -3,12 +3,12 @@ class HathiController < ApplicationController
   # ToDo Remove hathi_access_bib_status when requests and Orangelight will be updated 
   # with the new bib_id and oclc route hathi#hathi_access
   # Also remove it from the routes.rb
-  def hathi_access_bib_status
-    if params[:bib_id]
-      @record = HathiAccess.where(bibid: params[:bib_id])
-      render json: @record, except: [:id, :created_at, :updated_at]
-    end
-  end
+  # def hathi_access_bib_status
+#     if params[:bib_id]
+#       @record = HathiAccess.where(bibid: params[:bib_id])
+#       render json: @record, except: [:id, :created_at, :updated_at]
+#     end
+#   end
 
   def hathi_access
     return hathi_access_bib if params[:bib_id]
--- spec/controllers/hathi_controller_spec.rb
+++ spec/controllers/hathi_controller_spec.rb
@@ -10,33 +10,33 @@ RSpec.describe HathiController, type: :controller do
   end
 
   # ToDo Remove this test when the old route hathi#hathi_access_bib_status will be removed
-  describe '#hathi_access_bib_status' do
-    context 'when a bibid is provided' do
-      context 'when the bib exists' do
-        let(:bib_id) { "100" }
-        it 'returns an array of records with the same bib' do
-          get :hathi_access_bib_status, params: { bib_id: bib_id }
-
-          expect(JSON.parse(response.body).count).to eq 2
-          expect(JSON.parse(response.body)[0]["oclc_number"]).to eq("1234567")
-          expect(JSON.parse(response.body)[0]["bibid"]).to eq("100")
-          expect(JSON.parse(response.body)[0]["status"]).to eq("DENY")
-          expect(JSON.parse(response.body)[0]["origin"]).to eq("CUL")
-          expect(JSON.parse(response.body)[1]["oclc_number"]).to eq("2243200")
-          expect(JSON.parse(response.body)[1]["bibid"]).to eq("100")
-          expect(JSON.parse(response.body)[1]["status"]).to eq("ALLOW")
-          expect(JSON.parse(response.body)[1]["origin"]).to eq("CUL")
-          expect(response.status).to eq(200)
-        end
-      end
-      context 'when the bib does not exist' do
-        let(:bib_id) { "200" }
-        it 'returns an empty string' do
-          expect(response.body).to eq("")
-        end
-      end
-    end
-  end
+  # describe '#hathi_access_bib_status' do
+#     context 'when a bibid is provided' do
+#       context 'when the bib exists' do
+#         let(:bib_id) { "100" }
+#         it 'returns an array of records with the same bib' do
+#           get :hathi_access_bib_status, params: { bib_id: bib_id }
+#
+#           expect(JSON.parse(response.body).count).to eq 2
+#           expect(JSON.parse(response.body)[0]["oclc_number"]).to eq("1234567")
+#           expect(JSON.parse(response.body)[0]["bibid"]).to eq("100")
+#           expect(JSON.parse(response.body)[0]["status"]).to eq("DENY")
+#           expect(JSON.parse(response.body)[0]["origin"]).to eq("CUL")
+#           expect(JSON.parse(response.body)[1]["oclc_number"]).to eq("2243200")
+#           expect(JSON.parse(response.body)[1]["bibid"]).to eq("100")
+#           expect(JSON.parse(response.body)[1]["status"]).to eq("ALLOW")
+#           expect(JSON.parse(response.body)[1]["origin"]).to eq("CUL")
+#           expect(response.status).to eq(200)
+#         end
+#       end
+#       context 'when the bib does not exist' do
+#         let(:bib_id) { "200" }
+#         it 'returns an empty string' do
+#           expect(response.body).to eq("")
+#         end
+#       end
+#     end
+#   end
 
   describe '#hathi_access' do
     context 'when a bibid is provided' do
