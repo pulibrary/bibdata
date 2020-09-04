8a098be63a7dadbbee7296d8c5a36b17b23924ff
christinach <actspatial@gmail.com>
2020-09-04 08:48:33 -0400

    Add back the old route Update the name of the new route methods

--- app/controllers/hathi_controller.rb
+++ app/controllers/hathi_controller.rb
@@ -1,11 +1,18 @@
 class HathiController < ApplicationController
 
-  def hathi_access
-    return hathi_access_bib_status if params[:bib_id]
-    return hathi_access_oclc_status if params[:oclc]
+  def hathi_access_bib_status
+    if params[:bib_id]
+      @record = HathiAccess.where(bibid: params[:bib_id])
+      render json: @record, except: [:id, :created_at, :updated_at]
+    end
   end
 
-  def hathi_access_bib_status
+  def hathi_access
+    return hathi_access_bib if params[:bib_id]
+    return hathi_access_oclc if params[:oclc]
+  end
+
+  def hathi_access_bib
     @record = HathiAccess.where(bibid: params[:bib_id])
     if @record.present?
       render json: @record, except: [:id, :created_at, :updated_at]
@@ -14,7 +21,7 @@ class HathiController < ApplicationController
     end
   end
 
-  def hathi_access_oclc_status
+  def hathi_access_oclc
     @record = HathiAccess.where(oclc_number: params[:oclc])
     if @record.present?
       render json: @record, except: [:id, :created_at, :updated_at]
