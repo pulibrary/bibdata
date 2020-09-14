5a172d23feeb3cd14ec075b138da4097c245557e
Anna Headley <anna.headley@gmail.com>
2020-09-14 11:06:27 -0400

    Use :test queue_adapter in all tests
    Stops bibliographic_controller#update from trying to use
    sidekiq in tests. If other tests were using it they would fail on a
    redis not available error, so high confidence this is safe.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Don't use sidekiq in tests
+  config.active_job.queue_adapter = :test
 end
