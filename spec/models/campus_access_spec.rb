99fe99c7ec3d3bc8e60b94d01ece46a52d94cf6d
Carolyn Cole <cac9@princeton.edu>
2020-09-17 11:02:14 -0400

    Adding rake task to process daily access file Add method to class to load file Adds rake task to nicely run the class method fixes #789

---
+++ spec/models/campus_access_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe CampusAccess, type: :model do
+  let(:uid) { 'user123' }
+
+  describe '#has_access?' do
+    it 'does not find a user without access' do
+      expect(described_class.has_access?(uid)).to be_falsey
+    end
+
+    context 'with an record present' do
+      let(:uid) { 'admin123' }
+      before do
+        described_class.create(uid: uid)
+      end
+
+      it 'finds a user with access' do
+        expect(described_class.has_access?(uid)).to be_truthy
+      end
+    end
+  end
+
+  describe '#to_csv' do
+    before do
+      described_class.create(uid: 'user1')
+      described_class.create(uid: 'user2')
+    end
+    it 'creates a csv' do
+      expect(described_class.to_csv).to eq("user1\nuser2\n")
+    end
+  end
+
+  describe "#load_access" do
+    it "loads the database with the xslx file removing existing rows" do
+      CampusAccess.create(uid: 'abc123')
+      f=File.expand_path("../../fixtures/access.xlsx",__FILE__)
+      described_class.load_access(f)
+      expect(CampusAccess.count).to eq(4)
+      expect(CampusAccess.all.map(&:uid)).to contain_exactly("TEST1", "TEST2", "TEST3", "TEST6")
+    end
+
+    it "leaves the database alone if the file does not exist" do
+      CampusAccess.create(uid: 'abc123')
+      f=File.expand_path("../../fixtures/access2.xlsx",__FILE__)
+      described_class.load_access(f)
+      expect(CampusAccess.count).to eq(1)
+      expect(CampusAccess.all.map(&:uid)).to contain_exactly("abc123")
+    end
+  end
+end
