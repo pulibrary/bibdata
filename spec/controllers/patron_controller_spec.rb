f2d7a5e5b413179103d27e1fb4bafab294a91a02
Carolyn Cole <cac9@princeton.edu>
2020-09-17 14:44:49 -0400

    Adding campus access into patron data from voyager

--- spec/controllers/patron_controller_spec.rb
+++ spec/controllers/patron_controller_spec.rb
@@ -37,6 +37,18 @@ RSpec.describe PatronController, type: :controller do
       allow(controller).to receive(:current_user) { user }
       get :patron_info, params: { patron_id: 'steve', format: :json }
       expect(response).to have_http_status(200)
+      expect(response.body).to eq("{\"netid\":\"steve\",\"first_name\":\"Steven\",\"last_name\":\"Smith\",\"barcode\":\"00000000000000\",\"barcode_status\":1,\"barcode_status_date\":\"2013-10-17T16:11:29.000-05:00\",\"university_id\":\"000000000\",\"patron_group\":\"staff\",\"purge_date\":\"2016-10-31T23:00:06.000-05:00\",\"expire_date\":\"2017-10-31T23:00:06.000-05:00\",\"patron_id\":\"0000\",\"campus_authorized\":false}")
+    end
+
+    it "allows authenticated users to access patron info and includes campus access" do
+      CampusAccess.create(uid: 'steve')
+      stub_patron('steve')
+      user = double('user')
+      allow(request.env['warden']).to receive(:authenticate!) { user }
+      allow(controller).to receive(:current_user) { user }
+      get :patron_info, params: { patron_id: 'steve', format: :json }
+      expect(response).to have_http_status(200)
+      expect(response.body).to eq("{\"netid\":\"steve\",\"first_name\":\"Steven\",\"last_name\":\"Smith\",\"barcode\":\"00000000000000\",\"barcode_status\":1,\"barcode_status_date\":\"2013-10-17T16:11:29.000-05:00\",\"university_id\":\"000000000\",\"patron_group\":\"staff\",\"purge_date\":\"2016-10-31T23:00:06.000-05:00\",\"expire_date\":\"2017-10-31T23:00:06.000-05:00\",\"patron_id\":\"0000\",\"campus_authorized\":true}")
     end
   end
 
