5d72f07f26575a3268a3ff90d4f4ef3f486c6522
carolyncole <1599081+carolyncole@users.noreply.github.com>
2020-09-17 14:02:11 -0400

    Adding a route to get the list of patrons allowed on campus (#796)
    Updated to_csv to put in emails and lowercase since that is what libcal needs
    fixes #730

---
+++ spec/controllers/campus_access_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe CampusAccessController, type: :controller do
+
+  describe "#index" do
+    it "lists the users with campus access in csv format" do
+      CampusAccess.create(uid: 'ABC123')      
+      CampusAccess.create(uid: 'def456')      
+      CampusAccess.create(uid: 'ghi789')      
+      get :index, params: { format: :csv }
+      expect(response.body).to eq("abc123@princeton.edu\ndef456@princeton.edu\nghi789@princeton.edu\n")
+    end
+  end
+end
