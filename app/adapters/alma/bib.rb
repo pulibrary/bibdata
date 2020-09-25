d4f2b72caacc3db5cbcea1ca7d0c7ff9113be929
christinach <actspatial@gmail.com>
2020-09-25 08:03:38 -0400

    Clean unused let(s) in spec, Clean fixtures, Remove mulitple bibs logic from get_bib_record and create get_bib_records for multiple bibs #803 Move doc_count_unsuppressed to a different method, Update get_holding_records spec Adds test for requesting a suppressed bib Remove ids_remove_spaces method Move apikey, unsuppressed_marc, doc_count_unsuppressed, doc_unsuppressed to private

--- app/adapters/alma/bib.rb
+++ app/adapters/alma/bib.rb
@@ -1,33 +1,38 @@
 module Alma
   class Bib
-    # Get /almaws/v1/bibs Retrieve bibs
-    # @param ids [string] one or more ids. e.g ids = "991227850000541, 991227840000541 ,991227830000541" or ids = "991227830000541"
-    # @param mms_id [string]
-    # @param apikey [string]
-    # @param view [string]. The default is full. Use brief to retrieve without the full record
-    # @param expand [String]. Expands the bibliographic record with: p_avail - Expands physical inventory information, 
-    # e_avail - Expands electronic inventory information, 
-    # d_avail - Expand digital inventory information, 
-    # requests - Expand total number of title requests.
-    # get one bib record is supported in the bibdata UI and in the bibliographic_controller
-    # @param records an array of MARC::Record records
     class << self
-      def get_bib_record(ids, conn=nil, opts={})
-        res = Alma::Connector.connection.get("bibs?mms_id=#{self.ids_remove_spaces(ids: ids)}",
-        {query: { :expand => "p_avail,e_avail,d_avail,requests" }, :apikey => self.apikey},
+      # Get /almaws/v1/bibs Retrieve bibs
+      # @param id [String] e.g. id = "991227830000541"
+      # @param _opts not used in the Alma API
+      # @param _conn not used in the Alma API
+      # @see https://developers.exlibrisgroup.com/console/?url=/wp-content/uploads/alma/openapi/bibs.json#/Catalog/get%2Falmaws%2Fv1%2Fbibs Values that could be passed to the alma API
+      # get one bib record is supported in the bibdata UI and in the bibliographic_controller
+      # @return [MARC::Record]
+      def get_bib_record(id, conn=nil, opts={})
+        res = Alma::Connector.connection.get("bibs?mms_id=#{id}",
+        {query: { :expand => "p_avail,e_avail,d_avail,requests" }, :apikey => apikey},
         {'Accept' => 'application/xml'} )
 
         doc = Nokogiri::XML(res.body)
-        doc_unsuppressed = doc_unsuppressed(doc)
-        reader = MARC::XMLReader.new(StringIO.new(doc_unsuppressed.at_xpath('//bibs').to_xml))
-
-        return reader.first if doc_unsuppressed.xpath('//mms_id').count < 2
-        records = []
-        reader.select {|record| records << record}
+        doc_unsuppressed(doc)
+        unsuppressed_marc.first
       end
 
-      def doc_unsuppressed(doc)
-        doc.search('//bib').each {|node| node.remove if node.xpath('suppress_from_publishing').text == 'true'}
+      # Get /almaws/v1/bibs Retrieve bibs
+      # @param ids [Array] e.g. ids = ["991227850000541","991227840000541","99222441306421"]
+      # @param _opts not used in the Alma API
+      # @param _conn not used in the Alma API
+      # @see https://developers.exlibrisgroup.com/console/?url=/wp-content/uploads/alma/openapi/bibs.json#/Catalog/get%2Falmaws%2Fv1%2Fbibs Values that could be passed to the alma API
+      # @return [Array<MARC::Record>]
+      def get_bib_records(ids, conn=nil, opts={})
+        res = Alma::Connector.connection.get("bibs?mms_id=#{ids_array_to_string(ids)}",
+        {query: { :expand => "p_avail,e_avail,d_avail,requests" }, :apikey => apikey},
+        {'Accept' => 'application/xml'} )
+
+        doc = Nokogiri::XML(res.body)
+        doc_unsuppressed(doc)
+
+        unsuppressed_marc.to_a
       end
 
       # Returns list of holding records for a given MMS
@@ -39,13 +44,22 @@ module Alma
         doc = res.body
       end
 
-      # apikey only to read alma bibs.
-      def apikey
-        Alma.config[:bibs_read_only]
+      private
+
+      def doc_unsuppressed(doc)
+        @doc_unsuppressed = doc.search('//bib').each {|node| node.remove if node.xpath('suppress_from_publishing').text == 'true'}
       end
 
-      def ids_remove_spaces(ids:)
-        ids.gsub(/\s+/,"")
+      def unsuppressed_marc
+        MARC::XMLReader.new(StringIO.new(@doc_unsuppressed.at_xpath('//bibs').to_xml))
+      end
+
+      def ids_array_to_string(ids)
+        ids.join(",")
+      end
+
+      def apikey
+        Alma.config[:bibs_read_only]
       end
 
     end
--- spec/adapters/alma/bib_spec.rb
+++ spec/adapters/alma/bib_spec.rb
@@ -2,73 +2,74 @@
 require "rails_helper"
 
 RSpec.describe Alma::Bib do
-  let(:bibs) { "991227850000541, 991227840000541, 991227830000541, 99222441306421" }
-  let(:unsuppressed_991227850000541) { "991227850000541" }
-  let(:suppressed_99222441306421) { "99222441306421" }
-  let(:suppressed_unsuppressed_ids) { "991227850000541,991227840000541,99222441306421" }
-  let(:unsuppressed_xml_991227850000541) { file_fixture("alma/unsuppressed_#{unsuppressed_991227850000541}.xml").read }
+  let(:unsuppressed) { "991227850000541" }
+  let(:unsuppressed_two) { "991227840000541" }
+  let(:suppressed) { "99222441306421" }
+  let(:suppressed_unsuppressed_ids) { ["991227850000541" , "991227840000541", "99222441306421"] }
+  let(:suppressed_xml) { file_fixture("alma/suppressed_#{suppressed}.xml").read }
+  let(:unsuppressed_xml) { file_fixture("alma/unsuppressed_#{unsuppressed}.xml").read }
   let(:unsuppressed_suppressed) { file_fixture("alma/unsuppressed_suppressed.xml").read }
-  let(:suppressed_xml_99222441306421) { file_fixture("alma/suppressed_#{suppressed_99222441306421}.xml").read }
-  let(:alma_marc_991227850000541) { MARC::XMLReader.new(StringIO.new(unsuppressed_xml_991227850000541)).first }
+  let(:alma_marc_991227850000541) { MARC::XMLReader.new(StringIO.new(unsuppressed_xml)).first }
   let(:holdings_991227840000541) { file_fixture("alma/991227840000541_holdings.xml").read }
-  # let(:holdings_record) { MARC::XMLReader.new(StringIO.new(holdings_991227840000541)).first }
-  let(:unsuppressed_xml) { file_fixture("alma/unsuppressed.xml").read }
-  let(:records) { [] }
-  let(:alma_records) { file_fixture("alma/alma_three_records.xml").read }
-  let(:alma_marc_records) { MARC::XMLReader.new(StringIO.new(alma_records)).select {|record| records << record} }
 
   before do
-    Alma.config[:bibs_read_only] = 'TESTME'
+    allow(described_class).to receive(:apikey).and_return('TESTME')
     Alma.config[:region]='ALMA'
-    stub_request(:get, "https://ALMA/almaws/v1/bibs?apikey=TESTME&mms_id=991227850000541,991227840000541,99222441306421&query%5Bexpand%5D=p_avail,e_avail,d_avail,requests").
+    stub_request(:get, "https://ALMA/almaws/v1/bibs?apikey=TESTME&mms_id=#{suppressed_unsuppressed_ids.join(",")}&query%5Bexpand%5D=p_avail,e_avail,d_avail,requests").
        to_return(status: 200, body: unsuppressed_suppressed, headers: {
         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'Content-Type'=>'application/xml;charset=UTF-8',
         'Accept' => 'application/xml',
-        'User-Agent'=>'Faraday v1.0.1',
-        'Apikey' => 'TESTME'
+        'User-Agent'=>'Faraday v1.0.1'
       })
-    stub_request(:get, "https://ALMA/almaws/v1/bibs?apikey=TESTME&mms_id=#{suppressed_99222441306421}&query%5Bexpand%5D=p_avail,e_avail,d_avail,requests").
-       to_return(status: 200, body: suppressed_xml_99222441306421, headers: {
+    stub_request(:get, "https://ALMA/almaws/v1/bibs?apikey=TESTME&mms_id=#{suppressed}&query%5Bexpand%5D=p_avail,e_avail,d_avail,requests").
+       to_return(status: 200, body: suppressed_xml, headers: {
         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'Content-Type'=>'application/xml;charset=UTF-8',
         'Accept' => 'application/xml',
-        'User-Agent'=>'Faraday v1.0.1',
-        'Apikey' => 'TESTME'
+        'User-Agent'=>'Faraday v1.0.1'
       })
-    stub_request(:get, "https://ALMA/almaws/v1/bibs?apikey=TESTME&mms_id=#{unsuppressed_991227850000541}&query%5Bexpand%5D=p_avail,e_avail,d_avail,requests").
-         to_return(status: 200, body: unsuppressed_xml_991227850000541, headers: {
+    stub_request(:get, "https://ALMA/almaws/v1/bibs?apikey=TESTME&mms_id=#{unsuppressed}&query%5Bexpand%5D=p_avail,e_avail,d_avail,requests").
+         to_return(status: 200, body: unsuppressed_xml, headers: {
          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
          'Content-Type'=>'application/xml;charset=UTF-8',
          'Accept' => 'application/xml',
-         'User-Agent'=>'Faraday v1.0.1',
-         'Apikey' => 'TESTME'
+         'User-Agent'=>'Faraday v1.0.1'
+       })
+     stub_request(:get, "https://alma/almaws/v1/bibs/991227850000541/holdings?apikey=TESTME").
+         to_return(status: 200, body: holdings_991227840000541, headers: {
+           'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+           'Content-Type'=>'application/xml;charset=UTF-8',
+           'Accept' => 'application/xml',
+           'User-Agent'=>'Faraday v1.0.1'
        })
   end
   describe "#get_bib_record" do
-    context "if one bib is provided" do
+    context "when an unsuppressed bib is provided" do
       it "returns one record" do
-        expect(described_class.get_bib_record(unsuppressed_991227850000541)['001'].value).to eq "991227850000541"
+        expect(described_class.get_bib_record(unsuppressed)['001'].value).to eq "991227850000541"
       end
     end
-    context "if an array of bibs is provided" do
-      it "returns multiple unsuppressed records" do
-        expect(described_class.get_bib_record(suppressed_unsuppressed_ids)[0]['001'].value).to eq "991227840000541"
-        expect(described_class.get_bib_record(suppressed_unsuppressed_ids)[1]['001'].value).to eq "991227850000541"
-        expect(described_class.get_bib_record(suppressed_unsuppressed_ids).count).to eq 2
+    context "when a suppressed bib is provided" do
+      it "returns nil" do
+        expect(described_class.get_bib_record(suppressed)).to be nil
       end
     end
   end
-  describe "#ids_remove_spaces" do
-    it "removes the spaces from the ids" do
-      expect(described_class.ids_remove_spaces(ids: bibs)).to eq "991227850000541,991227840000541,991227830000541,99222441306421"
+
+  describe "#get_bib_records" do
+    context "if a string of bibs is provided" do
+      it "returns multiple unsuppressed records" do
+        expect(described_class.get_bib_records(suppressed_unsuppressed_ids)[0]['001'].value).to eq unsuppressed_two
+        expect(described_class.get_bib_records(suppressed_unsuppressed_ids)[1]['001'].value).to eq unsuppressed
+        expect(described_class.get_bib_records(suppressed_unsuppressed_ids).count).to eq 2
+      end
     end
   end
 
   describe '#get_holding_records' do
     it "returns the holdings for a bib" do
-      allow(described_class).to receive(:get_holding_records).with(unsuppressed_991227850000541).and_return(holdings_991227840000541)
-      expect(described_class.get_holding_records(unsuppressed_991227850000541)).to be_a(String)
+      expect(described_class.get_holding_records(unsuppressed)).to be_a(String)
     end
   end
 
