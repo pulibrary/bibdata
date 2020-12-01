d25666058e85398e463fb2b638853004e286346d
Trey Pendragon <tpendragon@princeton.edu>
2020-12-01 10:24:41 -0800

    Remove VoyagerHelpers, mark all dependent tests as pending Alma.

--- app/jobs/bib_dump_job.rb
+++ app/jobs/bib_dump_job.rb
@@ -1,13 +1,12 @@
-require 'voyager_helpers'
-
 class BibDumpJob < ActiveJob::Base
   queue_as :default
 
   def perform(id_slice, df_id)
-    df = DumpFile.find(df_id)
-    File.truncate(df.path, 0) if File.exist?(df.path)
-    VoyagerHelpers::Liberator.dump_bibs_to_file(id_slice, df.path)
-    df.zip
-    df.save
+    # TODO: Re-enable. Disabled as we no longer have VoyagerHelpers.
+    # df = DumpFile.find(df_id)
+    # File.truncate(df.path, 0) if File.exist?(df.path)
+    # VoyagerHelpers::Liberator.dump_bibs_to_file(id_slice, df.path)
+    # df.zip
+    # df.save
   end
 end
