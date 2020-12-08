b7b7f006ce6b0c82a4d0aa40179d0978f583c241
Trey Pendragon <tpendragon@princeton.edu>
2020-12-07 16:30:41 -0800

    Add beginning of availability response for :id

---
+++ app/adapters/alma_adapter/availability_status.rb
@@ -0,0 +1,40 @@
+class AlmaAdapter
+  class AvailabilityStatus
+    def self.from_bib(bib:)
+      new(bib: bib)
+    end
+
+    attr_reader :bib
+    def initialize(bib:)
+      @bib = bib
+    end
+
+    def to_h
+      holdings.each_with_object({}) do |holding, acc|
+        acc[holding["holding_id"]] = holding_summary(holding)
+      end
+    end
+
+    def holding_summary(holding)
+      {
+        more_items: holding["total_items"].to_i > 1,
+        location: marc_record["852"]["b"], # Location of bib record
+        item_id: item_data[holding["holding_id"]]&.first&.item_data&.fetch("pid", nil)
+      }
+    end
+
+    def item_data
+      @item_data ||= Alma::BibItem.find(bib.id).items.group_by do |item|
+        item["holding_data"]["holding_id"]
+      end
+    end
+
+    def marc_record
+      @marc_record ||= MARC::XMLReader.new(StringIO.new(bib.response["anies"].join(""))).to_a.first
+    end
+
+    def holdings
+      @availability_response ||= Alma::AvailabilityResponse.new(Array.wrap(bib)).availability[bib.id][:holdings]
+    end
+  end
+end
