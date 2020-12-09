3cd88d3df5495e26f5da0c22c497ec2b5a173aac
Trey Pendragon <tpendragon@princeton.edu>
2020-12-09 09:35:34 -0800

    Remove comment, add some documentation.

--- app/adapters/alma_adapter/status.rb
+++ app/adapters/alma_adapter/status.rb
@@ -1,5 +1,12 @@
+# Abstraction responsible for calculating the status and status label of a given
+# holding.
+# @TODO Add support for all the statuses in
+#   https://github.com/pulibrary/marc_liberation/issues/937
 class AlmaAdapter::Status
   attr_reader :bib, :holding_item_data, :holding
+  # @param bib [Alma::Bib]
+  # @param holding_item_data [Alma::BibItem]
+  # @param holding [Hash] Holding data pulled from Alma::AvailabilityResponse
   def initialize(bib:, holding_item_data:, holding:)
     @bib = bib
     @holding_item_data = holding_item_data
