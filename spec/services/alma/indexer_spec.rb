29d527b6434a9f65d48229e5b130c3eac17522d2
Trey Pendragon <tpendragon@princeton.edu>
2020-12-10 16:39:34 -0800

    Add Full Reindexer class.
    This uses the same strategy of downloading from itself as what currently
    happens, but it has a test and happens outside of a rake task.

--- spec/services/alma/indexer_spec.rb
+++ spec/services/alma/indexer_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Alma::Indexer do
-  describe "#full_reindex" do
-    it "gets the latest full dump tar, unzips it, and indexes all the containing files" do
+  describe "#full_reindex!" do
+    it "gets the latest full dump tar files, unzips it, and indexes all the contained files" do
       event = FactoryBot.create(:full_dump_event)
-      binding.pry
+      solr_url = ENV["SOLR_URL"] || "http://#{ENV['lando_marc_liberation_test_solr_conn_host']}:#{ENV['lando_marc_liberation_test_solr_conn_port']}/solr/marc-liberation-core-test"
+      stub_request(:get, "http://www.example.com/dump_files/spec%2Ffixtures%2Ffiles%2Falma%2Ffull_dump%2F1.xml.gz")
+        .to_return(status: 200, body: file_fixture("alma/full_dump/1.xml.gz").read, headers: {})
+      stub_request(:get, "http://www.example.com/dump_files/spec%2Ffixtures%2Ffiles%2Falma%2Ffull_dump%2F2.xml.gz")
+        .to_return(status: 200, body: file_fixture("alma/full_dump/2.xml.gz").read, headers: {})
+      solr = RSolr.connect(url: solr_url)
+      solr.delete_by_query("*:*")
+
+      indexer = described_class.new(solr_url: solr_url)
+      indexer.full_reindex!
+
+      solr.commit
+      response = solr.get("select", params: { q: "*:*" })
+      # There's one record in 1.xml, and one record in 2.xml
+      expect(response['response']['numFound']).to eq 2
     end
   end
 end
