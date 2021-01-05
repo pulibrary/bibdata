aaac59409ecdcdfa30b9c733b3cf48e94ae668ff
Trey Pendragon <tpendragon@princeton.edu>
2021-01-05 15:01:52 -0800

    Index holdings for Alma.
    This bypasses requiring figuring out the location values for now.

--- spec/marc_to_solr/lib/config_spec.rb
+++ spec/marc_to_solr/lib/config_spec.rb
@@ -48,6 +48,13 @@ describe 'From traject_config.rb' do
       record = @indexer.map_record(fixture_alma_record('9918573506421'))
       access_links = record["electronic_access_1display"]
       expect(JSON.parse(access_links.first)).to eq("http://dx.doi.org/10.1007/BFb0088073" => ["dx.doi.org"])
+    it "can index holdings" do
+      record = @indexer.map_record(fixture_alma_record('99211662100521'))
+      holdings = JSON.parse(record["holdings_1display"][0])
+      holding = holdings["2281813540006421"]
+      expect(holding["location"]).to eq "Periodicals"
+      expect(holding["library"]).to eq "Main Library"
+      expect(holding["location_code"]).to eq "MAIN-per-room"
     end
   end
   describe 'the cataloged_date from publishing job' do
