e9484f6d6bd0806135f0227f2b46606a61b1abd7
Trey Pendragon <tpendragon@princeton.edu>
2021-01-05 10:56:54 -0800

    Add HTTPS protocol to production URLs.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV["APPLICATION_URL"] || "bibdata.princeton.edu" }
+  config.action_mailer.default_url_options = { host: ENV["APPLICATION_URL"] || "bibdata.princeton.edu", protocol: "https" }
 end
