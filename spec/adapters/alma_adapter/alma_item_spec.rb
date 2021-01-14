63f8e24624d2868deba23f38af81fc66a75853a8
Trey Pendragon <tpendragon@princeton.edu>
2021-01-13 16:16:47 -0800

    Add ReCAP Customer Code

--- spec/adapters/alma_adapter/alma_item_spec.rb
+++ spec/adapters/alma_adapter/alma_item_spec.rb
@@ -1,22 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe AlmaAdapter::AlmaItem do
+  def build_item(code:)
+    Alma::BibItem.new(
+      "item_data" => {
+        "library" => {
+          "desc" => "ReCAP", "value" => "recap"
+        },
+        "location" => {
+          "desc" => code,
+          "value" => code
+        }
+      }
+    )
+  end
+
+  describe "#recap_customer_code" do
+    context "when location starts with x" do
+      it "returns PG" do
+        item = described_class.new(
+          build_item(code: "xx")
+        )
+
+        expect(item.recap_customer_code).to eq "PG"
+      end
+    end
+    context "when location doesn't start with X" do
+      it "capitalizes the code" do
+        item = described_class.new(
+          build_item(code: "pa")
+        )
+
+        expect(item.recap_customer_code).to eq "PA"
+      end
+    end
+  end
   describe "#group_designation" do
     ['pa', 'gp', 'qk', 'pf'].each do |code|
       context "when location is #{code}" do
         it "returns Shared" do
           item = described_class.new(
-            Alma::BibItem.new(
-              "item_data" => {
-                "library" => {
-                  "desc" => "ReCAP", "value" => "recap"
-                },
-                "location" => {
-                  "desc" => code,
-                  "value" => code
-                }
-              }
-            )
+            build_item(code: code)
           )
           expect(item.group_designation).to eq "Shared"
         end
@@ -26,17 +50,7 @@ RSpec.describe AlmaAdapter::AlmaItem do
       context "when location is #{code}" do
         it "returns Private" do
           item = described_class.new(
-            Alma::BibItem.new(
-              "item_data" => {
-                "library" => {
-                  "desc" => "ReCAP", "value" => "recap"
-                },
-                "location" => {
-                  "desc" => code,
-                  "value" => code
-                }
-              }
-            )
+            build_item(code: code)
           )
 
           expect(item.group_designation).to eq "Private"
@@ -49,17 +63,7 @@ RSpec.describe AlmaAdapter::AlmaItem do
       context "when location is #{code}" do
         it "returns In Library Use" do
           item = described_class.new(
-            Alma::BibItem.new(
-              "item_data" => {
-                "library" => {
-                  "desc" => "ReCAP", "value" => "recap"
-                },
-                "location" => {
-                  "desc" => code,
-                  "value" => code
-                }
-              }
-            )
+            build_item(code: code)
           )
 
           expect(item.recap_use_restriction).to eq "In Library Use"
@@ -71,17 +75,7 @@ RSpec.describe AlmaAdapter::AlmaItem do
       context "when location is #{code}" do
         it "returns Supervised Use" do
           item = described_class.new(
-            Alma::BibItem.new(
-              "item_data" => {
-                "library" => {
-                  "desc" => "ReCAP", "value" => "recap"
-                },
-                "location" => {
-                  "desc" => code,
-                  "value" => code
-                }
-              }
-            )
+            build_item(code: code)
           )
 
           expect(item.recap_use_restriction).to eq "Supervised Use"
