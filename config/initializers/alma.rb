a44f5e8372d295e7d3030bb002c506d54a5ff787
Eliot Jordan <eliotj@princeton.edu>
2021-01-14 13:28:08 -0600

    Use single production API key and set alma region variable in .env file

--- config/initializers/alma.rb
+++ config/initializers/alma.rb
@@ -1,5 +1,5 @@
 Alma.configure do |config|
-  config.apikey = Rails.configuration.alma[:bibs_read_only]
+  config.apikey = Rails.configuration.alma[:read_only_apikey]
 
   # Alma gem defaults to querying Ex Libris's North American API servers. You can override that here.
   # config.region   = "https://api-eu.hosted.exlibrisgroup.com"
