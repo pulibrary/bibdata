502f089ffc459c494d3c15e291d68558193f764c
Trey Pendragon <tpendragon@princeton.edu>
2021-01-13 16:09:08 -0800

    Add recap group designation.

--- spec/adapters/alma_adapter/alma_item_spec.rb
+++ spec/adapters/alma_adapter/alma_item_spec.rb
@@ -1,10 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe AlmaAdapter::AlmaItem do
+  describe "#group_designation" do
+    ['pa', 'gp', 'qk', 'pf'].each do |code|
+      context "when location is #{code}" do
+        it "returns Shared" do
+          item = described_class.new(
+            Alma::BibItem.new(
+              "item_data" => {
+                "library" => {
+                  "desc" => "ReCAP", "value" => "recap"
+                },
+                "location" => {
+                  "desc" => code,
+                  "value" => code
+                }
+              }
+            )
+          )
+          expect(item.group_designation).to eq "Shared"
+        end
+      end
+    end
+    ['jq', 'pe', 'pg', 'ph', 'pq', 'qb', 'ql', 'qv', 'qx'].each do |code|
+      context "when location is #{code}" do
+        it "returns Private" do
+          item = described_class.new(
+            Alma::BibItem.new(
+              "item_data" => {
+                "library" => {
+                  "desc" => "ReCAP", "value" => "recap"
+                },
+                "location" => {
+                  "desc" => code,
+                  "value" => code
+                }
+              }
+            )
+          )
+
+          expect(item.group_designation).to eq "Private"
+        end
+      end
+    end
+  end
   describe "#recap_use_restriction" do
-    context "when location is pj, pk, pl, pm, pn, or pt" do
-      it "returns In Library Use" do
-        ["pj", "pk", "pl", "pm", "pn", "pt"].each do |code|
+    ["pj", "pk", "pl", "pm", "pn", "pt"].each do |code|
+      context "when location is #{code}" do
+        it "returns In Library Use" do
           item = described_class.new(
             Alma::BibItem.new(
               "item_data" => {
@@ -20,12 +63,13 @@ RSpec.describe AlmaAdapter::AlmaItem do
           )
 
           expect(item.recap_use_restriction).to eq "In Library Use"
+          expect(item.group_designation).to eq "Private"
         end
       end
     end
-    context "when location is pb, ph, ps, pw, pz, xc, xg, xm, xn, xp, xr, xw, xx" do
-      it "returns Supervised Use" do
-        ["pb", "ph", "ps", "pw", "pz", "xc", "xg", "xm", "xn", "xp", "xr", "xw", "xx"].each do |code|
+    ["pb", "ph", "ps", "pw", "pz", "xc", "xg", "xm", "xn", "xp", "xr", "xw", "xx"].each do |code|
+      context "when location is #{code}" do
+        it "returns Supervised Use" do
           item = described_class.new(
             Alma::BibItem.new(
               "item_data" => {
@@ -41,6 +85,7 @@ RSpec.describe AlmaAdapter::AlmaItem do
           )
 
           expect(item.recap_use_restriction).to eq "Supervised Use"
+          expect(item.group_designation).to eq "Private"
         end
       end
     end
