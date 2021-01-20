fad1ecd20254121260c180e4b55efca69fe19d40
Anna Headley <anna.headley@gmail.com>
2021-01-20 15:36:10 -0500

    Use erb-interpreted config for seeds
    Instead of parsing it again / separately

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
 require 'yaml'
 
-cfg = YAML.safe_load(File.read(Rails.root.join('config/marc_liberation.yml')))
-
-cfg['dump_types'].each do |dt|
+MARC_LIBERATION_CONFIG['dump_types'].each do |dt|
   DumpType.create(label: dt['label'], constant: dt['constant'])
 end
 
-cfg['dump_file_types'].each do |dft|
+MARC_LIBERATION_CONFIG['dump_file_types'].each do |dft|
   DumpFileType.create(label: dft['label'], constant: dft['constant'])
 end
