3d820c31aea45d310d15814a0e9b651c7991f863
Anna Headley <anna.headley@gmail.com>
2021-01-26 16:46:56 -0500

    Generalize deprecated endpoints to be configurable as a route

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   get '/items/:item_id', to: 'items#item', defaults: { format: :json }
 
   get '/barcode', to: 'barcode#index', defaults: { format: :txt }
-  get '/barcode/:barcode', to: 'barcode#barcode', defaults: { format: :xml }
+  get '/barcode/:barcode', to: 'deprecated_endpoints#gone'
   get '/barcode/:barcode/scsb', to: 'barcode#scsb', defaults: { format: :xml }
 
   get '/codes/:location', to: 'codes#locations', defaults: { format: :json }
