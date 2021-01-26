74bda4a0bf01b3cff538659087426bc0cf441fd4
Anna Headley <anna.headley@gmail.com>
2021-01-26 16:46:56 -0500

    Generalize deprecated endpoints to be configurable as a route

---
+++ spec/features/deprecated_endpoints_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "deprecated endpoint page", type: :feature do
+  describe "any deprecated endpoint" do
+    it "returns 410 gone" do
+      visit "/barcode/32101044947941"
+      expect(page).to have_http_status(410)
+      expect(page).to have_content("Deprecated endpoint")
+    end
+  end
+end
