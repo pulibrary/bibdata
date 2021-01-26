9b72e18420dbb5314e0556737bb8a7582c7b536c
Anna Headley <anna.headley@gmail.com>
2021-01-26 16:56:52 -0500

    Deprecate the codes/:location endpoint

--- spec/routing/deprecated_endpoints_spec.rb
+++ spec/routing/deprecated_endpoints_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe "deprecated endpoint routes", type: :routing do
       expect(get: "/barcode/32101044947941").to route_to("deprecated_endpoints#gone", barcode: "32101044947941")
     end
   end
+
+  describe "codes/:location" do
+    it "routes to 410 gone" do
+      expect(get: "/codes/architecture").to route_to("deprecated_endpoints#gone", location: "architecture")
+    end
+  end
 end
