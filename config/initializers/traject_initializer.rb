d5c9397bc80e5ba2afdcb018012a712420b7b531
Eliot Jordan <eliotj@princeton.edu>
2021-01-28 12:23:44 -0600

    Extract indexer build method into new class

--- config/initializers/traject_initializer.rb
+++ config/initializers/traject_initializer.rb
@@ -1,4 +1,4 @@
 require File.join(Rails.root, 'marc_to_solr', 'lib', 'location_processor_service')
 
 LocationProcessorService.process unless Rails.env.test?
-TRAJECT_INDEXER ||= LocationProcessorService.build_indexer
+TRAJECT_INDEXER ||= IndexerService.build
