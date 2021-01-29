52fab13fa6377d01aaff47eb9c4e3908e19784c6
Eliot Jordan <eliotj@princeton.edu>
2021-01-28 19:17:17 -0600

    Fix config_spec

--- spec/support/required_environments.rb
+++ spec/support/required_environments.rb
@@ -1,4 +1,4 @@
-ENV["FIGGY_ARK_CACHE_PATH"] = "marc_to_solr/spec/fixtures/figgy_ark_cache"
+ENV["FIGGY_ARK_CACHE_PATH"] = "spec/fixtures/marc_to_solr/figgy_ark_cache"
 ENV["TRAJECT_CONFIG"] = "marc_to_solr/lib/traject_config.rb"
 ENV["BIBDATA_ADMIN_NETIDS"] = "admin123"
-ENV["HATHI_OUTPUT_DIR"] = "marc_to_solr/spec/fixtures/"
+ENV["HATHI_OUTPUT_DIR"] = "spec/fixtures/marc_to_solr/"
