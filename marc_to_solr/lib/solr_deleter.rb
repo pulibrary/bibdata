ffa0c216d463fb1b67706b7b7aff24586a7f4a2a
Hector Correa <hector_correa@princeton.edu>
2021-02-17 15:51:40 -0500

    Added tests and issues deletes in batches

--- marc_to_solr/lib/solr_deleter.rb
+++ marc_to_solr/lib/solr_deleter.rb
@@ -1,7 +1,7 @@
 require "net/http"
 require "json"
 
-# This class is to allow us to delete Solr records with our current 
+# This class is to allow us to delete Solr records with our current
 # version of Traject. Once we upgrade to Traject version 3.1 or later
 # we could use Traject's native delete feature
 # (see https://github.com/traject/traject/commit/c731d09c6cb15fbe572e4a294a7a127b525b7277)
@@ -13,15 +13,22 @@ class SolrDeleter
   end
 
   def delete(ids, batch_size = 100)
-    return if ids.count == 0
-    # TODO: break the update into batches
-    url = @solr_url + "/update?commit=true&wt=json"
-    payload = "<delete>" + ids.map {|id| "<id>#{id}</id>"}.join("") + "</delete>"
-    content_type = "text/xml" # request payload is in XML (even if the response is in JSON)
-    http_post(url, payload, content_type)
+    ids.each_slice(batch_size) do |batch|
+      response = delete_batch(batch)
+      raise StandardError, "Error deleting Solr documents. Status: #{response.code}. Body: #{response.body}" if response.code != "200"
+    end
   end
 
   private
+
+    def delete_batch(batch)
+      # Passes the ids to delete as a single XML document
+      url = @solr_url + "/update?commit=true&wt=json"
+      payload = "<delete>" + batch.map { |id| "<id>#{id}</id>" }.join("") + "</delete>"
+      content_type = "text/xml" # request payload is XML (even if the response is in JSON via the wt=json param)
+      http_post(url, payload, content_type)
+    end
+
     def http_post(url, payload, content_type)
       uri = URI.parse(url)
       http = Net::HTTP.new(uri.host, uri.port)
@@ -32,7 +39,6 @@ class SolrDeleter
       request = Net::HTTP::Post.new(uri.request_uri)
       request["Content-Type"] = content_type
       request.body = payload
-      response = http.request(request)
-      response        
+      http.request(request)
     end
 end
--- spec/services/alma/indexer_spec.rb
+++ spec/services/alma/indexer_spec.rb
@@ -55,6 +55,35 @@ RSpec.describe Alma::Indexer do
       # There's only one record in 2.xml
       expect(response['response']['numFound']).to eq 1
     end
+
+    it "handles deleted records" do
+      solr_url = ENV["SOLR_URL"] || "http://#{ENV['lando_marc_liberation_test_solr_conn_host']}:#{ENV['lando_marc_liberation_test_solr_conn_port']}/solr/marc-liberation-core-test"
+      solr = RSolr.connect(url: solr_url)
+      solr.delete_by_query("*:*")
+
+      indexer = described_class.new(solr_url: solr_url)
+
+      # Indexes a file with 5 non-deleted records
+      file_name = file_fixture("alma/incremental_11_records.xml")
+      indexer.index_file(file_name)
+      solr.commit
+      response = solr.get("select", params: { q: "*:*" })
+      expect(response['response']['numFound']).to eq 5
+
+      # Forcefully add a record (id=99122238836006421)...
+      file_name = file_fixture("alma/incremental_01_record_add.xml")
+      indexer.index_file(file_name)
+      solr.commit
+      response = solr.get("select", params: { q: "id:99122238836006421" })
+      expect(response['response']['numFound']).to eq 1
+
+      # ...and make sure it is deleted
+      file_name = file_fixture("alma/incremental_01_record_delete.xml")
+      indexer.index_file(file_name)
+      solr.commit
+      response = solr.get("select", params: { q: "id:99122238836006421" })
+      expect(response['response']['numFound']).to eq 0
+    end
   end
 
   describe "incremental_index!" do
