b5fb3fedde58ab4c5bc69573dd4389c18c8f1e6e
Hector Correa <hector_correa@princeton.edu>
2021-02-18 11:06:25 -0500

    Uses Farday for HTTP requests. Logs deletes like regular Traject log message

--- marc_to_solr/lib/solr_deleter.rb
+++ marc_to_solr/lib/solr_deleter.rb
@@ -1,21 +1,22 @@
 require "net/http"
 require "json"
 
-# This class is to allow us to delete Solr records with our current
-# version of Traject. Once we upgrade to Traject version 3.1 or later
-# we could use Traject's native delete feature
+# This class allow us to delete Solr records with our current
+# version of Traject. Once we upgrade to Traject version 3.1
+# or later we could use Traject's native delete feature
 # (see https://github.com/traject/traject/commit/c731d09c6cb15fbe572e4a294a7a127b525b7277)
-# but keep in mind that this feature is still in experimental mode in Traject
-# and their implementation does not support batches.
+# but keep in mind that Traject's implementation is still in experimental mode
+# and it does not (yet) support batches.
 class SolrDeleter
-  def initialize(solr_url)
+  def initialize(solr_url, logger = nil)
     @solr_url = solr_url
+    @logger = logger
   end
 
   def delete(ids, batch_size = 100)
     ids.each_slice(batch_size) do |batch|
       response = delete_batch(batch)
-      raise StandardError, "Error deleting Solr documents. Status: #{response.code}. Body: #{response.body}" if response.code != "200"
+      raise StandardError, "Error deleting Solr documents. Status: #{response.status}. Body: #{response.body}" if response.status != 200
     end
   end
 
@@ -26,19 +27,7 @@ class SolrDeleter
       url = @solr_url + "/update?commit=true&wt=json"
       payload = "<delete>" + batch.map { |id| "<id>#{id}</id>" }.join("") + "</delete>"
       content_type = "text/xml" # request payload is XML (even if the response is in JSON via the wt=json param)
-      http_post(url, payload, content_type)
-    end
-
-    def http_post(url, payload, content_type)
-      uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      if url.start_with?("https://")
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request["Content-Type"] = content_type
-      request.body = payload
-      http.request(request)
+      @logger.info "Deleting #{payload}" if @logger
+      Faraday.post(url, payload, "Content-Type" => content_type)
     end
 end
