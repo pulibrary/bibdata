9eda8d1a5d10e4ed182eb8032af2857c2a94bcc2
Anna Headley <anna.headley@gmail.com>
2021-03-03 08:58:42 -0500

    Re-enable bib items endpoint 404 spec
    And update logic according to how alma objects will behave

--- spec/support/stub_alma.rb
+++ spec/support/stub_alma.rb
@@ -11,11 +11,11 @@ module AlmaStubbing
       .to_return(status: status, body: all_items_path, headers: { 'Content-Type' => 'application/json' })
   end
 
-  def stub_alma_bib_items(mms_id:, filename:)
+  def stub_alma_bib_items(mms_id:, status: 200, filename:)
     alma_path = Pathname.new(file_fixture_path).join("alma")
     stub_request(:get, "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/#{mms_id}/holdings/ALL/items?expand=due_date_policy,due_date&order_by=library&direction=asc&limit=100")
       .to_return(
-        status: 200,
+        status: status,
         headers: { "content-Type" => "application/json" },
         body: alma_path.join(filename)
       )
