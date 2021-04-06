ab753a215fd280b63330c10342649d49c0f4ce49
Anna Headley <anna.headley@gmail.com>
2021-04-06 15:21:28 -0400

    Return 400 for availability params moved to new endpoints
    closes #1143

--- app/controllers/availability_controller.rb
+++ app/controllers/availability_controller.rb
@@ -4,50 +4,8 @@ class AvailabilityController < ApplicationController
   end
 
   def index
-    if params[:ids]
-      ids_param = sanitize_array(params[:ids])
-      avail = VoyagerLookup.multiple_bib_availability(bib_ids: ids_param)
-
-      if avail.empty?
-        render plain: "Record(s): #{ids_param} not found.", status: 404
-      else
-        respond_to do |wants|
-          wants.json  { render json: MultiJson.dump(avail) }
-        end
-      end
-    elsif params[:id]
-      id_param = sanitize(params[:id])
-      avail = adapter.get_availability(ids: [id_param])
-
-      if avail.empty?
-        render plain: "Record: #{id_param} not found.", status: 404
-      else
-        respond_to do |wants|
-          wants.json  { render json: MultiJson.dump(avail) }
-        end
-      end
-    elsif params[:mfhd]
-      mfhd_param = sanitize(params[:mfhd])
-      avail = VoyagerLookup.single_mfhd_availability(mfhd: mfhd_param.to_i)
-
-      if avail.empty?
-        render plain: "Record: #{mfhd_param} not found.", status: 404
-      else
-        respond_to do |wants|
-          wants.json  { render json: MultiJson.dump(avail) }
-        end
-      end
-    elsif params[:mfhd_serial]
-      mfhd_serial_param = sanitize(params[:mfhd_serial])
-      avail = VoyagerLookup.mfhd_serial_availability(mfhd_serial: mfhd_serial_param.to_i)
-
-      if avail.empty?
-        render plain: "No current issues found for record #{mfhd_serial_param}.", status: 404
-      else
-        respond_to do |wants|
-          wants.json  { render json: MultiJson.dump(avail) }
-        end
-      end
+    if params[:ids] || params[:id] || params[:mfhd] || params[:mfhd_serial]
+      render plain: "This endpoint no longer accepts this param", status: :bad_request
     elsif params[:barcodes]
       scsb_lookup = ScsbLookup.new
       avail = scsb_lookup.find_by_barcodes(sanitize_array(params[:barcodes]))
