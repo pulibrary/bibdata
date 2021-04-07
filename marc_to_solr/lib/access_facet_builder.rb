13be9b2273731637d6951be18e9125e1a2c54a4e
Eliot Jordan <eliotj@princeton.edu>
2021-04-07 16:45:00 -0500

    Add access_facet values

---
+++ marc_to_solr/lib/access_facet_builder.rb
@@ -0,0 +1,64 @@
+# Class for building access_facet values
+class AccessFacetBuilder
+  # Build access facet
+  # @param record [MARC::Record]
+  # @param context [Traject::Indexer::Context]
+  # @return [Array<String>] access values
+  def self.build(record:, context:)
+    new(record: record, context: context).build
+  end
+
+  attr_reader :record, :context
+
+  # Constructor
+  # @param record [MARC::Record]
+  # @param context [Traject::Indexer::Context]
+  def initialize(record:, context:)
+    @record = record
+    @context = context
+  end
+
+  # @return [Array<String>] access values
+  def build
+    [
+      electronic_portfolio,
+      in_library,
+      hathi,
+      hathi_temp,
+      marc_indicator
+    ].uniq.compact
+  end
+
+  private
+
+    def electronic_portfolio
+      return 'Online' if context.output_hash['electronic_portfolio_s'].present?
+    end
+
+    def in_library
+      return 'In the Library' if context.output_hash['location_code_s'].present?
+    end
+
+    def hathi
+      return 'Online' if hathi_locations.include?('hathi')
+    end
+
+    def hathi_locations
+      return [] unless context.output_hash['hathi_identifier_s'].present?
+      hathi_line = find_hathi_by_oclc(context.output_hash['oclc_s'].first)
+      parse_locations_from_hathi_line(hathi_line)
+    end
+
+    def hathi_temp
+      return 'Temporary Digital Access' if hathi_locations.include?('hathi_temp')
+    end
+
+    # Return 'online' if record has an 856 field and
+    # it's second indicator is 0, 1, or blank
+    def marc_indicator
+      field = record.find { |f| f.tag == '856' }
+      indicator = field.try(:indicator2)
+
+      return 'Online' if indicator == '0' || indicator == '1' || indicator == ' '
+    end
+end
