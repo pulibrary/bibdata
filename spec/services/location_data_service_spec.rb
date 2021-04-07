82ae9deb69a15ead5a80561c6fae4bb98443d94e
christinach <actspatial@gmail.com>
2021-04-07 08:54:36 -0400

    Add LocationToFileService to create holding and delivery json files add alma voyager mapping file, add holding_locations and delivery_locations files Update LocationDataService to populate holding locations from alma and alma voyager mapping file Update LocatonDataService to populate delivery locations using the delivery json file Update holding_locations with delivery locations Update location, library fixtures Add LocationToFile spec

--- spec/services/location_data_service_spec.rb
+++ spec/services/location_data_service_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe LocationDataService, type: :service do
         body: file_fixture("alma/locations1.json")
       )
 
-    stub_request(:get, "https://api-na.hosted.exlibrisgroup.com/almaws/v1/conf/libraries/main/locations")
+    stub_request(:get, "https://api-na.hosted.exlibrisgroup.com/almaws/v1/conf/libraries/annex/locations")
       .with(
         headers: {
           'Accept' => 'application/json',
@@ -52,20 +52,40 @@ RSpec.describe LocationDataService, type: :service do
         test_location.library = test_library
         test_location.save
       end
+      Locations::DeliveryLocation.new(label: 'test', address: 'test') do |_test_location|
+        test_delivery_location.library = test_library
+        test_delivery_location.save
+      end
     end
+
+    # delivery_record.label = delivery_location['label']
+    # delivery_record.address = delivery_location['address']
+    # delivery_record.phone_number = delivery_location['phone_number']
+    # delivery_record.contact_email = delivery_location['contact_email']
+    # delivery_record.staff_only =  delivery_location['staff_only']
+    # delivery_record.library = library_record
+    # delivery_record.gfa_pickup = delivery_location['gfa_pickup']
+    # delivery_record.pickup_location = delivery_location['pickup_location']
+    # delivery_record.digital_location = delivery_location['digital_location']
 
     it "deletes existing data and populates library and location data from Alma" do
       LocationDataService.delete_existing_and_repopulate
-      library_record = Locations::Library.find_by(code: 'main')
-      location_record1 = Locations::HoldingLocation.find_by(code: 'main$stacks')
-      location_record2 = Locations::HoldingLocation.find_by(code: 'arch$reserves')
+      library_record = Locations::Library.find_by(code: 'arch')
+      location_record1 = Locations::HoldingLocation.find_by(code: 'arch$stacks')
+      location_record2 = Locations::HoldingLocation.find_by(code: 'annex$stacks')
 
       expect(Locations::Library.count).to eq 2
-      expect(Locations::HoldingLocation.count).to eq 13
-      expect(library_record.label).to eq 'Main Library'
-      expect(location_record1.label).to eq 'Main Library'
+      expect(Locations::HoldingLocation.count).to eq 26
+      expect(library_record.label).to eq 'Architecture Library'
+      expect(location_record2.label).to eq 'Forrestal Annex - Annex Stacks'
       expect(location_record1.open).to be true
       expect(location_record2.open).to be false
+    end
+
+    it "deletes existing delivery locations table and populates new from json file" do
+      LocationDataService.delete_existing_and_repopulate
+      library_record = Locations::Library.find_by(code: 'annex')
+      expect(library_record.delivery_location_ids).to eq []
     end
   end
 end
