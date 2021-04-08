bc23e3c29a2f2af7e7aac2b73bfbcba8f40d513f
Eliot Jordan <eliotj@princeton.edu>
2021-04-08 11:52:20 -0500

    Remove unneeded access translation map

--- marc_to_solr/translation_maps/access.rb
+++
@@ -1,8 +0,0 @@
-{
-  "online$elf" => "Online",
-  "online$elf1" => "Online",
-  "online$elf3" => "Online",
-  "online$elf4" => "Online",
-  "hathi" => "Online",
-  "hathi_temp" => "Temporary Digital Access"
-}
