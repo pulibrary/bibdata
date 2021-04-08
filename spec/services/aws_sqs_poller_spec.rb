4a7f10d7b3556b6149a1c415072c26fbfe697e8e
Trey Pendragon <tpendragon@princeton.edu>
2021-04-08 13:34:35 -0700

    Handle SIGTERM for SQS Poller (#1224)
    * Handle SIGTERM for SQS Poller
    
    Prevents throwing an exception on Ctrl+C
    
    Closes #1091
    
    * Add comments.
    
    * One more comment.

--- spec/services/aws_sqs_poller_spec.rb
+++ spec/services/aws_sqs_poller_spec.rb
@@ -34,6 +34,31 @@ RSpec.describe AwsSqsPoller do
     Aws.config.clear
   end
 
+  context "when the process is killed" do
+    let(:poller_mock) do
+      Aws::SQS::QueuePoller.new(
+        "https://example.com"
+      )
+    end
+    let(:job_id) { "1434818870006421" }
+    let(:message_body) { JSON.parse(File.read(Rails.root.join('spec', 'fixtures', 'aws', 'sqs_full_dump.json'))).to_json }
+    it "doesn't throw an error, logs it, and ends polling" do
+      # Add the default signal handler back, RSpec's doesn't kill the process.
+      old_signal_handler = Signal.trap 'TERM', 'SYSTEM_DEFAULT'
+      # Force a process kill in AlmaDumpTransferJob
+      allow(AlmaDumpTransferJob).to receive(:perform_later) do
+        Process.kill 'TERM', 0
+      end
+
+      described_class.poll
+      # If it makes it this far polling didn't go infinitely and RSpec didn't
+      # get killed.
+
+      # Fix the signal handler.
+      Signal.trap 'TERM', old_signal_handler
+    end
+  end
+
   context "when a full dump job comes through" do
     let(:job_id) { "1434818870006421" }
     let(:message_body) { JSON.parse(File.read(Rails.root.join('spec', 'fixtures', 'aws', 'sqs_full_dump.json'))).to_json }
