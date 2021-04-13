4ba70a9e6174991e77d3f86a41a4d94635403043
Trey Pendragon <tpendragon@princeton.edu>
2021-04-13 13:40:49 -0700

    Delete unused code.

--- app/models/dump.rb
+++ app/models/dump.rb
@@ -19,100 +19,35 @@ class Dump < ActiveRecord::Base
     end
   end
 
-  def dump_updated_recap_records(updated_barcodes)
-    dump_file_type = DumpFileType.find_by(constant: 'RECAP_RECORDS')
-    dump_records(updated_barcodes, dump_file_type)
-  end
-
-  private
-
-    def dump_records(ids, dump_file_type, priority = 'default')
-      slice_size = if Rails.env.test?
-                     MARC_LIBERATION_CONFIG['test_records_per_file']
-                   elsif dump_file_type.constant == 'RECAP_RECORDS'
-                     MARC_LIBERATION_CONFIG['recap_dump_records_per_file']
-                   else
-                     MARC_LIBERATION_CONFIG['records_per_file']
-                   end
-      ids.each_slice(slice_size).each do |id_slice|
-        df = DumpFile.create(dump_file_type: dump_file_type)
-        self.dump_files << df
-        self.save
-        if dump_file_type.constant == 'RECAP_RECORDS'
-          RecapDumpJob.perform_later(id_slice, df.id)
-        else
-          BibDumpJob.set(queue: priority).perform_later(id_slice, df.id)
-        end
-        sleep 1
+  class << self
+    def partner_update
+      dump = nil
+      dump_type = 'PARTNER_RECAP'
+      timestamp = incremental_update_timestamp(dump_type)
+      Event.record do |event|
+        dump = Dump.create(dump_type: DumpType.find_by(constant: dump_type))
+        ScsbImportJob.perform_later(dump.id, timestamp)
+        dump.event = event
+        dump.save
       end
+      dump
     end
 
-    class << self
-      def dump_recap_records
-        dump_ids('PRINCETON_RECAP')
+    private
+
+      def last_recap_dump
+        dump_type = DumpType.where(constant: 'PRINCETON_RECAP')
+        Dump.where(dump_type: dump_type).joins(:event).where('events.success' => true).order('id desc').first
       end
 
-      def partner_update
-        dump = nil
-        dump_type = 'PARTNER_RECAP'
-        timestamp = incremental_update_timestamp(dump_type)
-        Event.record do |event|
-          dump = Dump.create(dump_type: DumpType.find_by(constant: dump_type))
-          ScsbImportJob.perform_later(dump.id, timestamp)
-          dump.event = event
-          dump.save
-        end
-        dump
+      def incremental_update_timestamp(dump_type)
+        (ENV['TIMESTAMP'] || last_incremental_update(dump_type) || DateTime.now - 1).to_time.strftime('%Y-%m-%d %H:%M:%S.%6N %z')
       end
 
-      private
-
-        def last_bib_id_dump
-          dump_type = DumpType.where(constant: 'BIB_IDS')
-          dump = Dump.where(dump_type: dump_type).joins(:event).where('events.success' => true).order('id desc').first
-        end
-
-        def last_recap_dump
-          dump_type = DumpType.where(constant: 'PRINCETON_RECAP')
-          dump = Dump.where(dump_type: dump_type).joins(:event).where('events.success' => true).order('id desc').first
-        end
-
-        def incremental_update_timestamp(dump_type)
-          (ENV['TIMESTAMP'] || last_incremental_update(dump_type) || DateTime.now - 1).to_time.strftime('%Y-%m-%d %H:%M:%S.%6N %z')
-        end
-
-        def last_incremental_update(dump_type)
-          last_dump = Dump.where(dump_type: DumpType.find_by(constant: dump_type)).last
-          last_dump = last_recap_dump if dump_type == "PRINCETON_RECAP"
-          last_dump&.created_at
-        end
-
-        def dump_ids(type)
-          # TODO: Re-enable. Disabled as we no longer have VoyagerHelpers.
-          # dump = nil
-          # Event.record do |event|
-          #   dump = Dump.create(dump_type: DumpType.find_by(constant: type))
-          #   dump.event = event
-          #   dump_file = DumpFile.create(dump: dump, dump_file_type: DumpFileType.find_by(constant: type)) unless type == 'PRINCETON_RECAP'
-          #   if type == 'BIB_IDS'
-          #     VoyagerHelpers::SyncFu.bib_ids_to_file(dump_file.path)
-          #   elsif type == 'PRINCETON_RECAP'
-          #     timestamp = incremental_update_timestamp(type).to_time
-          #     barcodes = VoyagerHelpers::SyncFu.recap_barcodes_since(timestamp)
-          #     dump.update_ids = barcodes
-          #     dump.save
-          #     dump.dump_updated_recap_records(barcodes)
-          #   else
-          #     raise 'Unrecognized DumpType'
-          #   end
-          #   unless type == 'PRINCETON_RECAP'
-          #     dump_file.save
-          #     dump_file.zip
-          #     dump.dump_files << dump_file
-          #   end
-          #   dump.save
-          # end
-          # dump
-        end
-    end # class << self
+      def last_incremental_update(dump_type)
+        last_dump = Dump.where(dump_type: DumpType.find_by(constant: dump_type)).last
+        last_dump = last_recap_dump if dump_type == "PRINCETON_RECAP"
+        last_dump&.created_at
+      end
+  end # class << self
 end
--- lib/tasks/sync.rake
+++ lib/tasks/sync.rake
@@ -3,9 +3,4 @@ namespace :marc_liberation do
   task partner_update: :environment do
     Dump.partner_update
   end
-
-  desc 'Dumps changed ReCAP records by barcode'
-  task recap_dump: :environment do
-    Dump.dump_recap_records
-  end
 end
