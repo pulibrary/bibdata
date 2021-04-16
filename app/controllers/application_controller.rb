60af8e13fc0dd4804e8b1baf3478d1df85356d92
Hector Correa <hector_correa@princeton.edu>
2021-04-16 16:18:16 -0400

    First pass at adding validation for PER_SECOND_THRESHOLD to the barcode endpoint.
    I added some helper methods to the ApplicationController that, if the approach looks
    good to the team, we can refactor the code in the BibliographicController to use them
    as well and make that controller a bit skinnier.
    
    Notice that I also moved some of the code in the BarcodeController to the AlmaAdapter
    so that the logic for "Alma stuff" is consolidated in a single place. This also masks
    the issue that Alma handles error reporting different from case to case: sometimes it
    raises an exception while other times (like in this case) it overloads the return object
    with the error information.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,20 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource_or_scope)
     request.referrer
   end
+
+  def handle_alma_exception(exception:, message:)
+    if exception.is_a?(Alma::PerSecondThresholdError)
+      Rails.logger.error "HTTP 429. #{message} #{exception}"
+      head :too_many_requests
+    elsif exception.is_a?(Alma::NotFoundError)
+      Rails.logger.error "HTTP 404. #{message} #{exception}"
+      head :not_found
+    elsif exception.is_a?(Alma::StandardError)
+      Rails.logger.error "HTTP 400. #{message} #{exception}"
+      head :bad_request
+    else
+      Rails.logger.error "HTTP 500. #{message} #{exception}"
+      head :internal_server_error
+    end
+  end
 end
