d9115ab978461a9b401980afc3141b7eb0acff84
Hector Correa <hector_correa@princeton.edu>
2021-04-20 16:15:56 -0400

    First pass at picking the correct status text from the work order, process type, and base status.

--- spec/adapters/alma_adapter_spec.rb
+++ spec/adapters/alma_adapter_spec.rb
@@ -376,9 +376,10 @@ RSpec.describe AlmaAdapter do
 
       # Test an actual response. These values are not particularly meaningful, but to make sure we don't
       # inadvertently change them when refactoring.
-      item_test = { barcode: "32101080920208", id: "23105104390006421", copy_number: "1", status: nil,
+      item_test = { barcode: "32101080920208", id: "23105104390006421", copy_number: "1",
+                    status: "Available", status_label: "Item in place",
                     on_reserve: "N", item_type: "Gen", pickup_location_id: "pa", pickup_location_code: "pa",
-                    location: "online$etasrcp", label: "ReCAP", in_temp_library: true, status_label: "Item in place",
+                    location: "online$etasrcp", label: "ReCAP", in_temp_library: true,
                     description: "g. 4, br. 7/8", enum_display: "g. 4, br. 7/8", chron_display: "",
                     temp_library_code: "online", temp_library_label: "Electronic Access",
                     temp_location_code: "online$etasrcp", temp_location_label: "Electronic Access" }
