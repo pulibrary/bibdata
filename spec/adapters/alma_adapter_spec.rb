dd3689812f78f0b463a6e092c780dfab5c7b9ed9
Hector Correa <hector_correa@princeton.edu>
2021-04-21 15:10:15 -0400

    Updated the logic to return the proper status based on the values from Alma

--- spec/adapters/alma_adapter_spec.rb
+++ spec/adapters/alma_adapter_spec.rb
@@ -376,7 +376,7 @@ RSpec.describe AlmaAdapter do
 
       # Test an actual response. These values are not particularly meaningful, but to make sure we don't
       # inadvertently change them when refactoring.
-      item_test = { barcode: "32101080920208", id: "23105104390006421", copy_number: "1",
+      item_test = { barcode: "32101080920208", id: "23105104390006421", holding_id: "22105104420006421", copy_number: "1",
                     status: "Available", status_label: "Item in place", status_source: "base_status",
                     on_reserve: "N", item_type: "Gen", pickup_location_id: "pa", pickup_location_code: "pa",
                     location: "online$etasrcp", label: "ReCAP", in_temp_library: true,
