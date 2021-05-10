961d78556a0e6e5db6d8b49757e64f77eee86a7c
Anna Headley <anna.headley@gmail.com>
2021-05-10 13:39:35 -0400

    Move a controller test to a requests test
    Co-authored-by: Bess Sadler <bess@ibiblio.org>
    Co-authored-by: Francis Kayiwa <kayiwa@pobox.com>
    Co-authored-by: Trey Pendragon <tpendragon@princeton.edu>

--- spec/requests/campus_access_spec.rb
+++ spec/requests/campus_access_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe CampusAccessController, type: :controller do
-  describe "#index" do
+RSpec.describe 'Campus Access Requests', type: :request do
+  describe "GET /campus_access.csv" do
     it "lists the users with campus access in csv format" do
       CampusAccess.create(uid: 'ABC123')
       CampusAccess.create(uid: 'def456')
       CampusAccess.create(uid: 'ghi789')
-      get :index, params: { format: :csv }
+
+      get '/campus_access.csv'
       expect(response.body).to eq("abc123@princeton.edu\ndef456@princeton.edu\nghi789@princeton.edu\n")
     end
   end
