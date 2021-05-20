2ede0b9ba99f8576b7c598de21e1c1632cbfdd9c
Eliot Jordan <eliotj@princeton.edu>
2021-05-20 18:13:06 -0500

    Run RecapDumpFileProcessingJobs in serial

--- spec/jobs/recap_dump_processing_job_spec.rb
+++ spec/jobs/recap_dump_processing_job_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 RSpec.describe RecapDumpProcessingJob do
   describe ".perform" do
+    before do
+      allow(RecapDumpFileProcessingJob).to receive(:perform_now)
+    end
+
     it "enqueues a RecapDumpFileProcessingJob for each DumpFile" do
       dump = FactoryBot.create(:recap_incremental_dump)
 
       described_class.perform_now(dump)
-      expect(RecapDumpFileProcessingJob).to have_been_enqueued.twice
+      expect(RecapDumpFileProcessingJob).to have_received(:perform_now).twice
       expect(RecapBoundwithsProcessingJob).to have_been_enqueued
     end
   end
