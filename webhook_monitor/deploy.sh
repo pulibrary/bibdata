3a483e758770af74356ba1593d823210cca92e2f
Trey Pendragon <tpendragon@princeton.edu>
2021-05-20 12:49:12 -0700

    Fix secret name for staging.

--- webhook_monitor/deploy.sh
+++ webhook_monitor/deploy.sh
@@ -12,7 +12,7 @@ elif [ $1 == "staging" ]
 then
   sam deploy --stack-name=alma-webhook-monitor-staging \
   --s3-prefix=alma-webhook-monitor \
-  --parameter-overrides='StageName="staging", QueueName="AlmaBibExportStaging.fifo", SecretID="alma/staging/webhookSecret"' \
+  --parameter-overrides='StageName="staging", QueueName="AlmaBibExportStaging.fifo", SecretID="alma/sandbox/webhookSecret"' \
   --s3-bucket=aws-sam-cli-managed-default-samclisourcebucket-1j1ve93v4jqs9 \
   --region='us-east-1' \
   --capabilities='CAPABILITY_IAM' \
