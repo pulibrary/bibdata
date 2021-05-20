a1aa35f5b0df0bc7a1bda1cbc64ececeed1c8a51
Eliot Jordan <eliotj@princeton.edu>
2021-05-20 18:13:06 -0500

    Run RecapDumpFileProcessingJobs in serial

--- app/jobs/recap_dump_processing_job.rb
+++ app/jobs/recap_dump_processing_job.rb
@@ -1,10 +1,12 @@
 # Process recap dump files and create recap boundwiths dumpfile
 class RecapDumpProcessingJob < ActiveJob::Base
   def perform(dump)
-    dump.dump_files.each do |dump_file|
-      RecapDumpFileProcessingJob.perform_later(dump_file)
-    end
-
     RecapBoundwithsProcessingJob.perform_later(dump)
+
+    dump.dump_files.each do |dump_file|
+      # Dump files must be processed serially for performance
+      # reasons on production infrastructure.
+      RecapDumpFileProcessingJob.perform_now(dump_file)
+    end
   end
 end
