937b09dd6b9332e6c9fd07a16f0fcf70264daaa4
Anna Headley <anna.headley@gmail.com>
2021-05-25 15:25:40 -0400

    Create event using Event.record
    fixes #1351

--- app/jobs/scsb_import_full_job.rb
+++ app/jobs/scsb_import_full_job.rb
@@ -1,8 +1,9 @@
 class ScsbImportFullJob < ActiveJob::Base
   def perform
-    event = Event.create
-    event.dump = Dump.create(dump_type: DumpType.find_by(constant: 'PARTNER_RECAP_FULL'))
-    event.save
-    Scsb::PartnerUpdates.full(dump: event.dump)
+    Event.record do |event|
+      event.dump = Dump.create(dump_type: DumpType.find_by(constant: 'PARTNER_RECAP_FULL'))
+      event.save
+      Scsb::PartnerUpdates.full(dump: event.dump)
+    end
   end
 end
--- spec/jobs/scsb_import_full_job_spec.rb
+++ spec/jobs/scsb_import_full_job_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe ScsbImportFullJob do
   it 'creates an event' do
     allow(Scsb::PartnerUpdates).to receive(:full)
     expect { described_class.perform_now }.to change { Event.count }.by(1)
+    event = Event.last
+    expect(event.start).not_to be nil
+    expect(event.finish).not_to be nil
     expect(Event.last.dump.dump_type.constant).to eq 'PARTNER_RECAP_FULL'
     expect(Scsb::PartnerUpdates).to have_received(:full)
   end
