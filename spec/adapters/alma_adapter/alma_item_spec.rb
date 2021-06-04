7f964ec2d98eb2e50b02e3e614870dba829ad81f
Hector Correa <hector_correa@princeton.edu>
2021-06-04 16:56:54 -0400

    Returns not available when item is in Acquisitions. Notice that we handle two differen scenarios: when the item has a process_type and when it has a work order.

--- spec/adapters/alma_adapter/alma_item_spec.rb
+++ spec/adapters/alma_adapter/alma_item_spec.rb
@@ -122,4 +122,70 @@ RSpec.describe AlmaAdapter::AlmaItem do
       end
     end
   end
+
+  describe "status" do
+    let(:item_work_order_acq) do
+      Alma::BibItem.new(
+        "bib_data" => { "mms_id" => "99122455086806421" },
+        "holding_data" => { "holding_id" => "22477860740006421" },
+        "item_data" => {
+          "pid" => "23477860730006421",
+          "base_status" => { "value" => "0", "desc" => "Item not in place" },
+          "process_type" => { "value" => "WORK_ORDER_DEPARTMENT", "desc" => "In Process" },
+          "work_order_type" => { "value" => "AcqWorkOrder", "desc" => "Acquisitions and Cataloging" },
+          "work_order_at" => { "value" => "AcqDepttechserv", "desc" => "Acquisitions and Cataloging" }
+        }
+      )
+    end
+
+    let(:item_process_type_acq) do
+      Alma::BibItem.new(
+        "bib_data" => { "mms_id" => "9939075533506421" },
+        "holding_data" => { "holding_id" => "22194161030006421" },
+        "item_data" => {
+          "pid" => "23194161020006421",
+          "base_status" => { "value" => "0", "desc" => "Item not in place" },
+          "process_type" => { "value" => "ACQ", "desc" => "Acquisition" }
+        }
+      )
+    end
+
+    let(:item_base_status_in_place) do
+      Alma::BibItem.new(
+        "bib_data" => { "mms_id" => "9939075533506421" },
+        "holding_data" => { "holding_id" => "22194161030006421" },
+        "item_data" => { "pid" => "23194161020006421", "base_status" => { "value" => "1", "desc" => "Item in place" } }
+      )
+    end
+
+    let(:item_base_status_not_in_place) do
+      Alma::BibItem.new(
+        "bib_data" => { "mms_id" => "9939075533506421" },
+        "holding_data" => { "holding_id" => "22194161030006421" },
+        "item_data" => { "pid" => "23194161020006421", "base_status" => { "value" => "0", "desc" => "Item not in place" } }
+      )
+    end
+
+    it "handles items with work order in acquisitions" do
+      item = described_class.new(item_work_order_acq)
+      status = item.calculate_status
+      expect(status[:code]).to eq "Not Available"
+    end
+
+    it "handles items with process type in acquisitions" do
+      item = described_class.new(item_process_type_acq)
+      status = item.calculate_status
+      expect(status[:code]).to eq "Not Available"
+    end
+
+    it "handles items with base status (in place)" do
+      item = described_class.new(item_base_status_in_place)
+      expect(item.calculate_status[:code]).to eq "Available"
+    end
+
+    it "handles items with base status (not in place)" do
+      item = described_class.new(item_base_status_not_in_place)
+      expect(item.calculate_status[:code]).to eq "Not Available"
+    end
+  end
 end
