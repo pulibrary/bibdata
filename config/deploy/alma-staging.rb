e195d1c7ea25c5f997fc6854caeff2c1ddcfe1d3
christinach <actspatial@gmail.com>
2021-06-15 16:47:52 -0400

    Add bibdata-alma-worker-staging1 in deploy

--- config/deploy/alma-staging.rb
+++ config/deploy/alma-staging.rb
@@ -7,6 +7,7 @@
 set :stage, :production
 set :rails_env, 'production'
 server 'bibdata-alma-staging1.princeton.edu', user: 'deploy', roles: [:web, :app, :db, :worker, :cron, :cron_staging, :hr_cron, :poller]
+server 'bibdata-alma-worker-staging1.princeton.edu', user: 'deploy', roles: [:db, :worker, :cron, :cron_staging]
 
 # Extended Server Syntax
 # ======================
