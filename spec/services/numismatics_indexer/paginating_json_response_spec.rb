84d8511d4a71b4dc2929df8405c265cf7014f5b8
jrgriffiniii <jrgriffiniii@gmail.com>
2021-06-16 12:37:36 -0400

    Increasing the test coverage for NumismaticsIndexer

---
+++ spec/services/numismatics_indexer/paginating_json_response_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe NumismaticsIndexer::PaginatingJsonResponse do
+  subject(:paginating_response) { described_class.new(url: url) }
+
+  let(:url) { 'http://localhost.localdomain/solr' }
+
+  describe "#total" do
+    let(:total_count) { 1 }
+    let(:solr_response) do
+      {
+        pages: {
+          total_count: total_count
+        }
+      }
+    end
+    let(:response_body) do
+      {
+        response: solr_response
+      }
+    end
+
+    before do
+      stub_request(
+        :get,
+        "#{url}&page=1"
+      ).to_return(
+        status: 200,
+        headers: {
+          "Content-Type" => "application/json"
+        },
+        body: JSON.generate(response_body)
+      )
+    end
+
+    it "requests the total count for the Solr Documents in response" do
+      expect(paginating_response.total).to eq(total_count)
+    end
+  end
+end
