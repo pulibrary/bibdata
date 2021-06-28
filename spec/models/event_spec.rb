232c79d67ccd06deb08c2078a94c6abf2ecf1978
Anna Headley <anna.headley@gmail.com>
2021-06-28 13:40:45 -0400

    Delete unused code
    This work now goes through rake task marc_liberation:delete:events and
    the DeleteEventsJob
    
    Co-authored-by: Bess Sadler <bess@ibiblio.org>
    Co-authored-by: Trey Pendragon <tpendragon@princeton.edu>
    Co-authored-by: Francis Kayiwa <kayiwa@pobox.com>

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -13,22 +13,6 @@ RSpec.describe Event, type: :model do
     Event.destroy_all
   end
 
-  describe 'ALL_RECORDS dump' do
-    it 'removes stale dump events after completing dump, no errors when id is nil' do
-      # Dumps without event ids would cause full dumps to fail
-      # An event id is still required to delete events
-      # (hence there being 9 holding id dump events instead of 8)
-      ActiveJob::Base.queue_adapter = :test
-      dump = Dump.where(dump_type: DumpType.find_by(constant: 'BIB_IDS')).first
-      dump.event_id = nil
-      dump.save
-      Event.delete_old_events
-      expect(dump_count('BIB_IDS')).to eq 9
-      expect(dump_count('CHANGED_RECORDS')).to eq 8
-      expect(dump_count('ALL_RECORDS')).to eq 3
-    end
-  end
-
   describe 'Failed dump' do
     it 'creates an unsuccessful event' do
       expect(test_failed_event.success).to be false
@@ -64,9 +48,5 @@ RSpec.describe Event, type: :model do
     Event.record do |_event|
       raise Exception.new('test')
     end
-  end
-
-  def dump_count(type)
-    Dump.where(dump_type: DumpType.find_by(constant: type)).count
   end
 end
