61c5acd4e8e10d8c96f34da867f4d2c254218334
Trey Pendragon <tpendragon@princeton.edu>
2021-06-30 14:02:20 -0700

    Update test for incremental files to use real filenames.
    Co-authored-by: Anna Headley <anna.headley@gmail.com>

--- spec/models/scsb/partner_updates_spec.rb
+++ spec/models/scsb/partner_updates_spec.rb
@@ -99,11 +99,11 @@ RSpec.describe Scsb::PartnerUpdates, type: :model do
   describe '.incremental' do
     let(:file_type) { DumpFileType.find_by(constant: 'RECAP_RECORDS') }
     before do
-      FileUtils.cp('spec/fixtures/scsb_updates/updates.zip', update_directory_path)
-      FileUtils.cp('spec/fixtures/scsb_updates/deletes.zip', update_directory_path)
+      FileUtils.cp('spec/fixtures/scsb_updates/updates.zip', update_directory_path.join("CUL-NYPL_20210622_183200.zip"))
+      FileUtils.cp('spec/fixtures/scsb_updates/deletes.zip', update_directory_path.join("CUL-NYPL_20210622_183300.zip"))
       allow(bucket).to receive(:download_files).and_return(
-        [Rails.root.join(update_directory_path, 'updates.zip').to_s],
-        [Rails.root.join(update_directory_path, 'deletes.zip').to_s]
+        [Rails.root.join(update_directory_path, 'CUL-NYPL_20210622_183200.zip').to_s],
+        [Rails.root.join(update_directory_path, 'CUL-NYPL_20210622_183300.zip').to_s]
       )
     end
 
@@ -114,8 +114,8 @@ RSpec.describe Scsb::PartnerUpdates, type: :model do
       expect(dump.dump_files.where(dump_file_type: file_type).length).to eq(2)
       expect(dump.dump_files.where(dump_file_type: log_file_type).length).to eq(1)
       expect(dump.dump_files.map(&:path)).to contain_exactly(
-        File.join(scsb_file_dir, "scsbupdateupdates_1.xml.gz"),
-        File.join(scsb_file_dir, "scsbupdateupdates_2.xml.gz"),
+        File.join(scsb_file_dir, "scsbupdate_20210622_183200_1.xml.gz"),
+        File.join(scsb_file_dir, "scsbupdate_20210622_183200_2.xml.gz"),
         a_string_matching(/#{scsb_file_dir}\/fixes_\d{4}_\d{2}_\d{2}.json.gz/)
       )
 
