03c73e1fa6a7f7a556311e57bcf2e708a1c21450
Trey Pendragon <tpendragon@princeton.edu>
2021-07-08 11:09:55 -0700

    Add extra bibdata-alma-workers.
    Work towards #924

--- config/deploy/alma-production.rb
+++ config/deploy/alma-production.rb
@@ -3,3 +3,6 @@ set :rails_env, 'production'
 server 'bibdata-alma1.princeton.edu', user: 'deploy', roles: [:web, :app, :db, :worker]
 server 'bibdata-alma2.princeton.edu', user: 'deploy', roles: [:web, :app, :db, :worker, :hr_cron]
 server 'bibdata-alma-worker1.princeton.edu', user: 'deploy', roles: [:db, :worker, :cron, :cron_production]
+server 'bibdata-alma-worker2.princeton.edu', user: 'deploy', roles: [:db, :worker]
+server 'bibdata-alma-worker3.princeton.edu', user: 'deploy', roles: [:db, :worker]
+server 'bibdata-alma-worker4.princeton.edu', user: 'deploy', roles: [:db, :worker]
