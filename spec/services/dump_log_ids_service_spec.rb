92223dc3869207675da9e8377e9ac052a016a982
Hector Correa <hector_correa@princeton.edu>
2021-07-12 18:10:21 -0400

    Second pass. Reuse the files indicated in an existing dump (rather than creating new one). Ditch Traject and parse the MARC XML directly.

---
+++ spec/services/dump_log_ids_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ::DumpLogIdsService do
+  describe "#process_dump" do
+    it "gets the right counts" do
+      dump = FactoryBot.create(:incremental_dump)
+      service = described_class.new()
+      service.process_dump(dump.id)
+      dump = Dump.find(dump.id)
+      expect(dump.delete_ids.count).to eq 14
+      expect(dump.update_ids.count).to eq 7
+    end
+
+    it "validates the dump type" do
+      dump = FactoryBot.create(:recap_incremental_dump)
+      service = described_class.new()
+      expect { service.process_dump(dump.id) }.to raise_error(StandardError)
+    end
+  end
+end
