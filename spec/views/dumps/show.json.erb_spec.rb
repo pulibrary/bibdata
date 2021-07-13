81c6a6f7c52f434caafcafc84547f6cbdddffca2
Hector Correa <hector_correa@princeton.edu>
2021-07-13 12:29:33 -0400

    Added test

---
+++ spec/views/dumps/show.json.erb_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "dumps/show", type: :view do
+  context "dump show page" do
+    it "includes " do
+      assign(:dump, FactoryBot.create(:full_dump))
+      render
+      response = JSON.parse(rendered)
+      expect(response["type"]).to eq "all_records"
+      expect(response["generated_date"]).to eq "2021-07-13T15:00:00.000Z"
+    end
+  end
+end
