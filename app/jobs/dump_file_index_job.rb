190ef285de7b1617d03b14c535b338939f7a240f
Trey Pendragon <tpendragon@princeton.edu>
2021-07-13 15:12:16 -0700

    Updates pg/sidekiq
    This also wraps the DumpFileIndexJob in a connection to try to fix #1514

--- app/jobs/dump_file_index_job.rb
+++ app/jobs/dump_file_index_job.rb
@@ -1,5 +1,7 @@
 class DumpFileIndexJob < ActiveJob::Base
   def perform(dump_file, solr_url:)
-    Alma::Indexer::DumpFileIndexer.new(dump_file, solr_url: solr_url).index!
+    ActiveRecord::Base.connection_pool.with_connection do
+      Alma::Indexer::DumpFileIndexer.new(dump_file, solr_url: solr_url).index!
+    end
   end
 end
