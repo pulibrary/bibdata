d173e0e6f5b0c479895f122f52ec36c234bd5200
Hector Correa <hector_correa@princeton.edu>
2021-08-18 10:41:03 -0400

    Removes unused property from AlmaAdapter::Status class

--- app/adapters/alma_adapter/status.rb
+++ app/adapters/alma_adapter/status.rb
@@ -3,13 +3,12 @@
 # @TODO Add support for all the statuses in
 #   https://github.com/pulibrary/marc_liberation/issues/937
 class AlmaAdapter::Status
-  attr_reader :bib, :holding_item_data, :holding, :aeon
+  attr_reader :bib, :holding, :aeon
   # @param bib [Alma::Bib]
-  # @param holding_item_data [Alma::BibItem]
   # @param holding [Hash] Holding data pulled from Alma::AvailabilityResponse
-  def initialize(bib:, holding_item_data:, holding:, aeon: false)
+  # @param aeon [Bool] Is Aeon location?
+  def initialize(bib:, holding:, aeon: false)
     @bib = bib
-    @holding_item_data = holding_item_data
     @holding = holding
     @aeon = aeon
   end
