681b4355d8a27bd7488a9b627a045b0a8b76bf5d
Bess Sadler <bess.sadler@princeton.edu>
2021-10-04 14:28:46 -0400

    Augment subjects with Indigenous Studies where appropriate
    New indexing is feature flagged behind ENV['CHANGE_THE_SUBJECT'] == 'true'

---
+++ marc_to_solr/lib/augment_the_subject.rb
@@ -0,0 +1,111 @@
+# frozen_string_literal: true
+
+require 'set'
+
+##
+# The creation and management of metadata are not neutral activities.
+class AugmentTheSubject
+  LCSH_TERMS_FILE = File.join(File.dirname(__FILE__), 'augment_the_subject', 'indigenous_studies.txt')
+  LCSH_SUBFIELDS_FILE = File.join(File.dirname(__FILE__), 'augment_the_subject', 'indigenous_studies_subfields.json')
+
+  ##
+  # Ensure the needed config files exist
+  def initialize
+    raise "Cannot find lcsh terms file at #{LCSH_TERMS_FILE}" unless File.exist?(LCSH_TERMS_FILE)
+    raise "Cannot find lcsh subfields file at #{LCSH_SUBFIELDS_FILE}" unless File.exist?(LCSH_SUBFIELDS_FILE)
+  end
+
+  def indigenous_studies_terms
+    @indigenous_studies_terms || load_indigenous_studies_terms
+  end
+
+  def load_indigenous_studies_terms
+    @indigenous_studies_terms = Set.new
+    File.foreach(LCSH_TERMS_FILE) do |lcsh_term|
+      @indigenous_studies_terms << normalize(lcsh_term)
+    end
+    @indigenous_studies_terms
+  end
+
+  def indigenous_studies_subfields
+    @indigenous_studies_subfields || load_indigenous_studies_subfields
+  end
+
+  def load_indigenous_studies_subfields
+    @indigenous_studies_subfields = JSON.parse(File.read(LCSH_SUBFIELDS_FILE))
+    @indigenous_studies_subfields
+  end
+
+  ##
+  # Normalize lcsh terms so they can match at index time.
+  # 1. downcase
+  # 2. replace ǂ terms with SEPARATOR
+  def normalize(lcsh_term)
+    lcsh_term.chomp.downcase.gsub(/ ǂ. /, SEPARATOR)
+  end
+
+  ##
+  # Given an array of terms, add "Indigenous Studies" if any of the terms match
+  # @param [<String>] terms
+  # @return [<String>]
+  def add_indigenous_studies(terms)
+    terms << "Indigenous Studies" if indigenous_studies?(terms)
+    terms
+  end
+
+  ##
+  # Given an array of terms, check whether this set of terms should have an
+  # additional subject heading of "Indigenous Studies" added
+  # @param [<String>] terms
+  # @return [Boolean]
+  def indigenous_studies?(terms)
+    terms.each do |term|
+      return true if subfield_a_match?(term)
+      return true if full_subject_term_match?(term)
+    end
+    false
+  end
+
+  ##
+  # For some subject terms, only the first part needs to match.
+  # E.g., "Quinnipiac Indians-History", "Quinnipiac Indians-Culture" should both
+  # be assigned an Indigenous Studies term even though that entire term doesn't
+  # appear in our terms list.
+  def subfield_a_match?(term)
+    subfield_a = term.split(SEPARATOR).first
+    indigenous_studies_subfields["a"].include?(subfield_a)
+  end
+
+  ##
+  # For some subject terms, we need to match on all subfields
+  # E.g., Russia (Federation)-Civilization-Indian influences
+  def full_subject_term_match?(term)
+    indigenous_studies_terms.include? term.downcase.gsub(/[^\w\s#{SEPARATOR}]/, '')
+  end
+
+  ##
+  # DANGER ZONE
+  # This will re-parse the indigenous_studies.txt file into a json file that
+  # has split apart the marc subfields. A person will then need to go in and
+  # manually edit the json config file to remove irrelevant subjects.
+  def self.parse_subjects
+    subfield_a = Set.new
+    subfield_x = Set.new
+    subfield_y = Set.new
+    subfield_z = Set.new
+    File.foreach(LCSH_TERMS_FILE) do |lcsh_term|
+      subfield_a << lcsh_term.chomp.split('ǂ').first.strip
+      subfield_x << lcsh_term.chomp.split("ǂx").last.strip.split("ǂ").first.strip if lcsh_term =~ /ǂx/
+      subfield_y << lcsh_term.chomp.split("ǂy").last.strip.split("ǂ").first.strip if lcsh_term =~ /ǂy/
+      subfield_z << lcsh_term.chomp.split("ǂz").last.strip.split("ǂ").first.strip if lcsh_term =~ /ǂz/
+    end
+    output = {}
+    output[:a] = subfield_a.sort
+    output[:x] = subfield_x.sort
+    output[:y] = subfield_y.sort
+    output[:z] = subfield_z.sort
+    ## Uncomment this line to re-write the subfields file. Remember that you will then need to edit it by hand.
+    # File.open(lcsh_subfields_file, "w") { |f| f.write output.to_json }
+    output
+  end
+end
---
+++ spec/marc_to_solr/lib/augment_the_subject_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+##
+# When our catalog records contain subject headings, that should be classified as
+# Indigenous Studies, it adds that term.
+RSpec.describe AugmentTheSubject do
+  let(:ats) { described_class.new }
+
+  context "subject terms about Indigenous people" do
+    let(:subject_terms) { ["Indians of North America#{SEPARATOR}Connecticut", "Quinnipiac Indians."] }
+
+    it "are recognized" do
+      expect(ats.indigenous_studies?(subject_terms)).to eq true
+    end
+
+    it "adds Indigenous Studies as a subject" do
+      expect(ats.add_indigenous_studies(subject_terms)).to eq subject_terms << "Indigenous Studies"
+    end
+  end
+
+  context "subject term that are not relevant" do
+    let(:subject_terms) { ["Daffodils", "Tulips"] }
+
+    it "does not add Indigenous Studies as a subject" do
+      expect(ats.indigenous_studies?(subject_terms)).to eq false
+    end
+  end
+
+  context "mismatched capitalization" do
+    let(:subject_terms) { ["Indians OF NORTH America#{SEPARATOR}Connecticut", "Quinnipiac Indians."] }
+
+    it "matches anyway" do
+      expect(ats.indigenous_studies?(subject_terms)).to eq true
+    end
+  end
+
+  context "mismatched punctuation" do
+    let(:subject_terms) { ["Quinnipiac Indians."] }
+
+    it "matches anyway" do
+      expect(ats.indigenous_studies?(subject_terms)).to eq true
+    end
+  end
+
+  context "normalizing lcsh terms" do
+    it "replaces ǂz with SEPARATOR" do
+      lcsh_term = "Indian architecture ǂz North America"
+      expect(ats.normalize(lcsh_term)).to eq "indian architecture#{SEPARATOR}north america"
+    end
+  end
+
+  context "subfield ǂa matches with trailing subfields" do
+    let(:subject_terms) { ["Quinnipiac Indians#{SEPARATOR}History"] }
+
+    it "matches anyway" do
+      expect(ats.indigenous_studies?(subject_terms)).to eq true
+    end
+  end
+
+  context "initial setup" do
+    it "parses the raw list of subjects into subfields" do
+      subfields = described_class.parse_subjects
+      expect(subfields.keys).to match_array [:a, :x, :y, :z]
+      expect(subfields[:a].count).to eq 1341
+    end
+  end
+end
