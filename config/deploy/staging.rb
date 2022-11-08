51e9c81cb2e0ef95a9ecae49b039d606418fa409
Christina Chortaria <actspatial@gmail.com>
2022-11-08 15:44:00 -0500

    restart the worker only on the worker machine

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 set :application, 'bibdata'
 set :stage, :production
 set :rails_env, 'production'
-server 'bibdata-alma-staging1.princeton.edu', user: 'deploy', roles: [:web, :app, :db, :worker, :hr_cron, :poller]
+server 'bibdata-alma-staging1.princeton.edu', user: 'deploy', roles: [:web, :app, :db, :hr_cron, :poller]
 server 'bibdata-alma-worker-staging1.princeton.edu', user: 'deploy', roles: [:db, :worker, :cron, :cron_staging, :poller]
 
 # Extended Server Syntax
