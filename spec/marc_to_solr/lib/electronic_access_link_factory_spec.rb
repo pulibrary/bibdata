f539558231974700c17244c262ee7f606ff1e184
Jane Sandberg <js7389@princeton.edu>
2023-03-09 18:00:13 -0800

    Minor refactor of ElectronicAccessLinkFactory

---
+++ spec/marc_to_solr/lib/electronic_access_link_factory_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+RSpec.describe ElectronicAccessLinkFactory do
+  it 'can identify that a link is an ark' do
+    field = MARC::DataField.new('856', '4', '0',
+            MARC::Subfield.new('u', 'https://arks.princeton.edu/ark:/88435/wh246s44f'))
+    expect(described_class.build(bib_id: '123', marc_field: field).ark).to be_truthy
+  end
+end
