56216f5ea0998ed527c3017186e7ae11b9e34dc8
Jane Sandberg <sandbergja@users.noreply.github.com>
2023-03-13 14:16:09 -0700

    i2097: use File.read, not IO.read (#2098)
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@
 
 require "redis"
 
-redis_config = YAML.safe_load(ERB.new(IO.read(Rails.root.join("config", "redis.yml"))).result, aliases: true)[Rails.env].with_indifferent_access
+redis_config = YAML.safe_load(ERB.new(File.read(Rails.root.join("config", "redis.yml"))).result, aliases: true)[Rails.env].with_indifferent_access
 redis_client = Redis.new(redis_config.merge(thread_safe: true))._client
 
 Sidekiq::Client.reliable_push! unless Rails.env.test?
