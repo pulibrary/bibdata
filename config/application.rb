0d41f4a409686f8e9926ff6ec3493eadc1b24694
Max Kadel <mkadel@email.unc.edu>
2023-07-31 15:14:57 -0400

    Only require lando_env in dev and test envs (#2226)

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require_relative 'lando_env'
+require_relative 'lando_env' if Rails.env.development? || Rails.env.test?
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
