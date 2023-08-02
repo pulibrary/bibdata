b6a635234c8dc18681633c16abee190af436abe4
Haider <haider.ashfaq@7vals.com>
2023-08-02 18:02:00 +0500

    fix: removed blank test files

--- spec/routing/bibliographic_routing_spec.rb
+++
@@ -1,6 +0,0 @@
-require "rails_helper"
-
-RSpec.describe BibliographicController, type: :routing do
-  describe "routing" do
-  end
-end
--- spec/system/deprecated_endpoints_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "deprecated endpoint page", type: :system do
-  describe "any deprecated endpoint" do
-  end
-end
