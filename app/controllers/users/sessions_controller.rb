10ff740f58293e4da8402f02f70a69d977670aec
Jane Sandberg <js7389@princeton.edu>
2023-09-08 14:10:26 -0700

    Make signout message translateable

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Users::SessionsController < Devise::SessionsController
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    flash[:alert] = "<strong><a href='https://fed.princeton.edu/cas/logout'>Sign out from CAS</a></strong>"
+    flash[:alert] = I18n.t('users.signout_html')
     set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
     yield if block_given?
     respond_to_on_destroy
