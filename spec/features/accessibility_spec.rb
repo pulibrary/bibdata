7ab5fedd5c3e2b5ab44e1ce71c7cf2f87324ac60
Liz Garcia <109767065+lizgarciao@users.noreply.github.com>
2023-10-03 08:56:44 -0400

    Include axe accessibility tests to bibdata (#2258)
    * Include axe accessibility tests to bibdata
    
    * Include lang in html tag for layout

---
+++ spec/features/accessibility_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+describe "accessibility", type: :feature, js: true do
+  context "home page" do
+    before do
+      visit "/"
+    end
+
+    it "complies with wcag" do
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa)
+        .excluding('a[href$="events"]')
+        .excluding('a[href$="delivery_locations"]')
+        .excluding('a[href$="holding_locations"]')
+        .excluding('a[href$="libraries"]')
+        .excluding('.deployment-version')
+        .excluding('span')
+        .excluding('.form-group')
+        .excluding('input[value="1"][name="holdings_only"][type="hidden"]')
+        .excluding('a[rel="nofollow"]')
+    end
+  end
+end
