ba8f604a081b379381adba17391d7af905c158b1
Liz Garcia <lizgarova16@gmail.com>
2023-11-26 02:19:57 -0500

    Include 10 events in seeds of database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 seeder = DataSeeder.new
 seeder.generate_dump_types
 seeder.generate_dump_file_types
+
+10.times do |i|
+  Event.find_or_create_by(id: i+1, start: "200#{i}-10-10", finish: "200#{i}-10-11", success: true)
+end
