e445a234a232881b60c1fa9d97805d50a5c18863
Liz Garcia <lizgarova16@gmail.com>
2023-12-02 23:10:19 -0500

    include events without specifying id so that tests pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ seeder.generate_dump_types
 seeder.generate_dump_file_types
 
 10.times do |i|
-  Event.find_or_create_by(id: i+1, start: "200#{i}-10-10", finish: "200#{i}-10-11", success: true)
+  Event.find_or_create_by(start: "200#{i}-10-10", finish: "200#{i}-10-11", success: true)
 end
