0473b835941039a759d453ae1cffd6c41fb9cc5a
lizgarciao <lizgarova16@gmail.com>
2024-02-03 01:55:19 -0500

    update seeds file to run in development environment so specs run properly
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ seeder = DataSeeder.new
 seeder.generate_dump_types
 seeder.generate_dump_file_types
 
-10.times do |i|
-  Event.find_or_create_by(start: "200#{i}-10-10", finish: "200#{i}-10-11", success: true)
+if Event.count < 10 && Rails.env.development?
+  10.times do |i|
+    Event.find_or_create_by(start: "200#{i}-10-10", finish: "200#{i}-10-11", success: true)
+  end
 end
