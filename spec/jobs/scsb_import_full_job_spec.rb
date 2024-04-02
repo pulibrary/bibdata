7d88abbf1a942fe79aceda0c77feb25caa149fd0
Beck Davis <bd4538@princeton.edu>
2024-04-02 15:24:50 -0400

    Add test for stale files in the update directory path
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/jobs/scsb_import_full_job_spec.rb
+++ spec/jobs/scsb_import_full_job_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe ScsbImportFullJob do
     expect(event.dump.dump_type.constant).to eq 'PARTNER_RECAP_FULL'
     expect(Scsb::PartnerUpdates).to have_received(:full)
   end
+
+  describe 'when there stale files in the update directory path' do
+    let(:update_directory_path) { Rails.root.join("tmp", "specs", "update_directory") }
+
+    before do
+      allow(ENV).to receive(:[]).with('SCSB_PARTNER_UPDATE_DIRECTORY').and_return(update_directory_path)
+      FileUtils.cp('spec/fixtures/scsb_updates/CUL_20210429_192300.zip', update_directory_path)
+      FileUtils.cp('spec/fixtures/scsb_updates/NYPL_20210430_015000.zip', update_directory_path)
+      FileUtils.cp('spec/fixtures/scsb_updates/HL_20210716_063500.zip', update_directory_path)
+    end
+
+    it 'removes stale files' do
+      expect {one = one}
+    end
+  end  
 end
