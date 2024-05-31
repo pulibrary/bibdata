69ef7f3c8a0910de3f67ee62128122de6e524b60
Max Kadel <mkadel@princeton.edu>
2024-05-31 10:45:44 -0400

    Fix event seeds, address deprecation warning
    - The seeds were creating events without dates, since they were trying to create the year "20010", which the DB apparently doesn't believe in. This made it so that the events could not be viewed in the UI.
    - Address deprecation warning that was distracting from the real issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-if Event.count < 10 && Rails.env.development?
-  10.times do |i|
+if Event.count < 9 && Rails.env.development?
+  9.times do |i|
     Event.find_or_create_by(start: "200#{i}-10-10", finish: "200#{i}-10-11", success: true)
   end
 end
 
-if Dump.count < 10 && Rails.env.development?
+if Dump.count < 9 && Rails.env.development?
   Event.all.each do |event|
     event.dump = Dump.create!(dump_type: :changed_records)
     event.save
