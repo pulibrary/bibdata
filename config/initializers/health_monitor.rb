c350e57e593aeac34f2938d3d9084b78283e8c02
Max Kadel <mkadel@princeton.edu>
2024-07-24 15:41:29 -0400

    Add solr, DB, and cache health checks
    Closes #2409

---
+++ config/initializers/health_monitor.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+Rails.application.config.after_initialize do
+  HealthMonitor.configure do |config|
+    config.cache
+
+    config.add_custom_provider(SolrStatus)
+
+    # Make this health check available at /health
+    config.path = :health
+
+    config.error_callback = proc do |e|
+      Rails.logger.error "Health check failed with: #{e.message}"
+      Honeybadger.notify(e)
+    end
+  end
+end
