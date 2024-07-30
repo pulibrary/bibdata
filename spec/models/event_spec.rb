c5d1d0f9b8e035646003905065f821ea4a4fb7a5
Christina Chortaria <actspatial@gmail.com>
2024-07-30 14:50:03 -0400

    [#1929] Test that we cannot add duplicate events
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>
    Co-authored-by: regineheberlein <regineheberlein@users.noreply.github.com>

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,23 +2,40 @@ require 'rails_helper'
 require 'fileutils'
 
 RSpec.describe Event, type: :model do
-  before(:all) do
-    bibs = './spec/fixtures/sample_bib_ids.txt'
-    10.times { dump_test_bib_ids(bibs) }
-    10.times { test_events(:changed_records) }
-    5.times { test_events(:full_dump) }
-  end
-
-  after(:all) do
+  after do
     described_class.destroy_all
   end
 
   describe 'Failed dump' do
+    before do
+      bibs = './spec/fixtures/sample_bib_ids.txt'
+      10.times { dump_test_bib_ids(bibs) }
+      10.times { test_events(:changed_records) }
+      5.times { test_events(:full_dump) }
+    end
     it 'creates an unsuccessful event' do
       expect(test_failed_event.success).to be false
     end
   end
 
+  context "When attempting to create duplicate events" do
+    let(:message_body) { JSON.parse(File.read(Rails.root.join('spec', 'fixtures', 'aws', 'sqs_incremental_dump.json'))).to_json }
+
+    before do
+      described_class.create(message_body:)
+    end
+    it "does not create a duplicate event" do
+      expect do
+        described_class.create(message_body:)
+      end.not_to change { Event.count }
+      expect(Event.count).to eq 1
+    end
+    it "does not create a duplicate event and raises an error" do
+      expect do
+        described_class.create!(message_body:)
+      end.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Message body has already been taken')
+    end
+  end
   def dump_test_bib_ids(bibs)
     dump = nil
     Event.record do |event|
