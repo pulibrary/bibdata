a148b5a5f8ed0a034fee1476823cb24ca8ec0aa5
Christina Chortaria <actspatial@gmail.com>
2024-07-31 17:00:06 -0400

    [#2434] Pass the event.id in the scsb import full job Update shared spec to create a dump with an event_id
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/support/scsb_partner_updates_incremental.rb
+++ spec/support/scsb_partner_updates_incremental.rb
@@ -2,10 +2,12 @@
 
 RSpec.shared_context 'scsb_partner_updates_incremental' do
   include_context 'scsb_partner_updates'
-  let(:dump) { Dump.create(dump_type: :partner_recap) }
+  let(:event) { FactoryBot.build(:event) }
+  let(:dump) { Dump.create(dump_type: :partner_recap, event_id: event.id) }
   let(:timestamp) { Dump.send(:incremental_update_timestamp) }
   let(:scsb_file) { file_fixture("scsb/scsb_leaderd.xml").to_s }
   before do
+    event.save
     allow(s3_bucket).to receive(:list_files)
     FileUtils.cp(Rails.root.join(fixture_paths, 'updates.zip'), update_directory_path.join("CUL-NYPL-HL_20210622_183200.zip"))
     FileUtils.cp(Rails.root.join(fixture_paths, 'deletes.zip'), update_directory_path.join("CUL-NYPL-HL_20210622_183300.zip"))
