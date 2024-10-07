9f0a9331b1de2d7d4a91a305eaf48a1a5ff14a50
Jane Sandberg <sandbergja@users.noreply.github.com>
2024-10-07 09:08:13 -0700

    Refactor genre extraction into its own class (#2509)
    Helps with #2508

--- spec/marc_to_solr/lib/princeton_marc_spec.rb
+++ spec/marc_to_solr/lib/princeton_marc_spec.rb
@@ -504,50 +504,6 @@ describe 'From princeton_marc.rb' do
     end
   end
 
-  describe 'process_genre_facet function' do
-    before(:all) do
-      @g600 = { "600" => { "ind1" => "", "ind2" => "0", "subfields" => [{ "a" => "Exclude" }, { "v" => "John" }, { "x" => "Join" }] } }
-      @g630 = { "630" => { "ind1" => "", "ind2" => "0", "subfields" => [{ "x" => "Fiction." }] } }
-      @g655 = { "655" => { "ind1" => "", "ind2" => "0", "subfields" => [{ "a" => "Culture." }, { "x" => "Dramatic rendition" }, { "v" => "Awesome" }] } }
-      @g655_2 = { "655" => { "ind1" => "", "ind2" => "7", "subfields" => [{ "a" => "Poetry" }, { "x" => "Translations into French" }, { "v" => "Maps" }] } }
-      @g655_3 = { "655" => { "ind1" => "", "ind2" => "7", "subfields" => [{ "a" => "Manuscript" }, { "x" => "Translations into French" }, { "v" => "Genre" }, { "2" => "rbgenr" }] } }
-      @sample_marc = MARC::Record.new_from_hash('fields' => [@g600, @g630, @g655, @g655_2, @g655_3])
-      @genres = process_genre_facet(@sample_marc)
-    end
-
-    it 'trims punctuation' do
-      expect(@genres).to include("Culture")
-    end
-
-    it 'excludes $a when not 655' do
-      expect(@genres).not_to include("Exclude")
-    end
-
-    it 'excludes 2nd indicator of 7 if vocab type is not in approved list' do
-      expect(@genres).not_to include("Maps")
-      expect(@genres).not_to include("Poetry")
-    end
-
-    it 'includes 2nd indicator of 7 if vocab type is in approved list' do
-      expect(@genres).to include("Manuscript")
-      expect(@genres).to include("Genre")
-    end
-
-    it 'includes 6xx $v and 655 $a' do
-      expect(@genres).to include("John")
-      expect(@genres).to include("Awesome")
-    end
-
-    it 'includes 6xx $x from filtered in terms' do
-      expect(@genres).to include("Fiction")
-    end
-
-    it 'excludes $x terms that do not match filter list' do
-      expect(@genres).not_to include("Join")
-      expect(@genres).not_to include("Dramatic renditon")
-    end
-  end
-
   describe 'process_hierarchy function' do
     before(:all) do
       @s610_ind2_5 = { "600" => { "ind1" => "", "ind2" => "5", "subfields" => [{ "a" => "Exclude" }] } }
