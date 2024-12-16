fd542dc73ddc0be6f589458dc9ab190bda779b4b
Jane Sandberg <js7389@princeton.edu>
2024-12-16 09:12:18 -0800

    [#2552] Don't stop the SCSB import full job if stale file is already deleted

--- spec/jobs/scsb_import_full_job_spec.rb
+++ spec/jobs/scsb_import_full_job_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe ScsbImportFullJob do
     expect(Scsb::PartnerUpdates).to have_received(:full)
   end
 
-  describe 'when there stale files in the update directory path' do
+  describe 'when there are stale files in the update directory path' do
     let(:update_directory_path) { Rails.root.join("tmp", "specs", "update_directory") }
 
     before do
@@ -35,6 +35,21 @@ RSpec.describe ScsbImportFullJob do
       expect(File.file?(Rails.root.join("tmp", "specs", "update_directory", 'HL_20210716_063500.zip'))).to be false
       expect(File.file?(Rails.root.join("tmp", "specs", "update_directory", 'scsb_update_20240108_183400_1.xml'))).to be false
       expect(Dir.exist?(Rails.root.join("tmp", "specs", "update_directory"))).to be true
+    end
+
+    context 'when one file deletion fails' do
+      before do
+        allow(FileUtils).to receive(:rm).and_call_original
+        allow(FileUtils).to receive(:rm).with(Rails.root.join("tmp", "specs", "update_directory", 'NYPL_20210430_015000.zip').to_s).and_raise(Errno::ENOENT, 'No such file or directory @ apply2files')
+      end
+      it 'still deletes the other files that have not failed' do
+        described_class.perform_now
+
+        expect(FileUtils).to have_received(:rm).exactly(4).times
+        expect(File.file?(Rails.root.join("tmp", "specs", "update_directory", 'CUL_20210429_192300.zip'))).to be false
+        expect(File.file?(Rails.root.join("tmp", "specs", "update_directory", 'HL_20210716_063500.zip'))).to be false
+        expect(File.file?(Rails.root.join("tmp", "specs", "update_directory", 'scsb_update_20240108_183400_1.xml'))).to be false
+      end
     end
   end
 end
