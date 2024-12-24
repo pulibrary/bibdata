1d56b5b268998b38741e38342f6b76ffda617d0e
Max Kadel <mkadel@princeton.edu>
2024-12-24 13:37:07 -0500

    Change more jobs to be Sidekiq jobs

--- app/jobs/download_partner_files_job.rb
+++ app/jobs/download_partner_files_job.rb
@@ -1,8 +1,13 @@
 require 'json/add/regexp'
 
 # Downloads partner updates from S3, and kicks off the ProcessPartnerUpdatesJob if the files are successfully downloaded
-class DownloadPartnerFilesJob < ApplicationJob
-  def perform(file_filter:, dump_id:, file_prefix:)
-    Scsb::PartnerUpdates::Full.download_partner_files(file_filter:, dump_id:, file_prefix:)
+class DownloadPartnerFilesJob
+  include Sidekiq::Job
+  def perform(params)
+    Scsb::PartnerUpdates::Full.download_partner_files(
+      file_filter: params['file_filter'],
+      dump_id: params['dump_id'],
+      file_prefix: params['file_prefix']
+    )
   end
 end
