ac774bede3ed8acf009261d00b519e57ec77c672
Christina Chortaria <actspatial@gmail.com>
2024-12-31 15:21:29 -0500

    Remove unused voyager stub method Mark as pending tests that were previously marked as pending, using metadata pending Helps with the Rails 7.2 upgrade

--- spec/requests/barcode_gets_spec.rb
+++ spec/requests/barcode_gets_spec.rb
@@ -3,18 +3,14 @@ require 'marc'
 
 RSpec.describe 'Barcode Gets', type: :request do
   describe 'GET /barcode/32101070300312' do
-    it 'returns a collection of bibliographic records' do
-      pending 'Replace with Alma'
-      # stub_voyager_barcodes('32101070300312')
+    it 'returns a collection of bibliographic records', pending: 'Replace with Alma' do
       get '/barcode/32101070300312'
       expect(response.status).to be(200)
     end
   end
 
   describe 'GET /barcode/321010702214' do
-    it 'returns an error when the barcode is not a valid form' do
-      pending 'Replace with Alma'
-      # allow(VoyagerHelpers::Liberator).to receive(:get_records_from_barcode).and_return([])
+    it 'returns an error when the barcode is not a valid form', pending: 'Replace with Alma' do
       get '/barcode/321010702214'
       expect(response.status).to be(404)
     end
@@ -26,9 +22,4 @@ RSpec.describe 'Barcode Gets', type: :request do
       expect(response.status).to be(404)
     end
   end
-end
-
-def stub_voyager_barcodes(barcode)
-  f = File.expand_path("../../fixtures/#{barcode}.mrx", __FILE__)
-  allow(VoyagerHelpers::Liberator).to receive(:get_records_from_barcode).and_return MARC::XMLReader.new(f).to_a
 end
