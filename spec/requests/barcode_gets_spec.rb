1ccd8c187314008459ffb77f09c6b8c9c4758694
Christina Chortaria <actspatial@gmail.com>
2025-01-02 10:44:25 -0500

    Update config/test.rb with the correct value for config.action_dispatch.show_exceptions See: https://discuss.rubyonrails.org/t/behaviors-in-action-controllers-raising-error-has-been-changed-in-7-1/83933/2

--- spec/requests/barcode_gets_spec.rb
+++ spec/requests/barcode_gets_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe 'Barcode Gets', type: :request do
 
   describe 'GET /barcode/321010702214' do
     it 'returns a 404 when the barcode is not a valid form', pending: 'Replace with Alma' do
-      # skipping because while pending it fails and breaks CI
-      skip
       get '/barcode/321010702214'
       expect(response.status).to be(404)
     end
