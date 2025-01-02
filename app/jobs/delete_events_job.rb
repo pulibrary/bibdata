13aa85a9572b352dea3c38109d03d5828f37d9d6
Max Kadel <mkadel@princeton.edu>
2025-01-02 12:23:08 -0500

    Use sidekiq job for DeleteEventsJob

--- app/jobs/delete_events_job.rb
+++ app/jobs/delete_events_job.rb
@@ -1,9 +1,11 @@
-class DeleteEventsJob < ApplicationJob
+class DeleteEventsJob
+  include Sidekiq::Worker
   # @param dump_type [Symbol] the dump type of the dumps you want to clean up
   # @param older_than [int] the Time before which dumps should be deleted
-  def perform(dump_type:, older_than:)
+  def perform(dump_type, older_than)
+    older_than = Time.zone.parse(older_than)
     event_ids = Event.joins(:dump)
-                     .where('dump.dump_type': dump_type)
+                     .where('dump.dump_type': dump_type.to_sym)
                      .where(start: ..older_than)
                      .map(&:id)
     Event.destroy(event_ids)
