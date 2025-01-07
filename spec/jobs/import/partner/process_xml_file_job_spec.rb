0005d13cd723b3e19c18518ec41dd93132f5c2fc
Max Kadel <mkadel@princeton.edu>
2025-01-07 15:31:52 -0500

    Use different timing in CI and locally

--- spec/jobs/import/partner/process_xml_file_job_spec.rb
+++ spec/jobs/import/partner/process_xml_file_job_spec.rb
@@ -15,11 +15,16 @@ RSpec.describe Import::Partner::ProcessXmlFileJob do
     allow(File).to receive(:unlink)
   end
 
+  if ENV['CI']
+    let(:performance_time) { 165 }
+  else
+    let(:performance_time) { 75 }
+  end
   let(:large_xml_file) { Rails.root.join('spec/fixtures/scsb_updates/several_records.xml').to_s }
 
   it 'is reasonably performant' do
     expect do
       described_class.perform_async(dump.id, large_xml_file)
-    end.to perform_under(75).ms.sample(10).times
+    end.to perform_under(performance_time).ms.sample(10).times
   end
 end
