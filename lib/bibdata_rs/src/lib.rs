aa63ef95e2b22dad4ddaf23806c185a16fcf657b
Jane Sandberg <js7389@princeton.edu>
2025-05-04 20:44:48 -0700

    Start implementing XML deserialization and JSON serialization

--- lib/bibdata_rs/src/lib.rs
+++ lib/bibdata_rs/src/lib.rs
@@ -1,13 +1,46 @@
 use magnus::{function, prelude::*, Error, Ruby};
+use serde::{ser::SerializeStruct, Deserialize, Serialize, Serializer};
+use std::fs;
 
-fn hello() -> String {
-    "toaster".to_owned()
+#[derive(Deserialize)]
+struct Metadata {
+    #[serde(rename = "oai_dc:dc")]
+    thesis: Thesis,
 }
 
+#[derive(Debug, Deserialize)]
+struct Thesis {
+    #[serde(rename = "dc:title")]
+    title: Vec<String>,
+}
+
+impl Serialize for Thesis {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut serializer = serializer.serialize_struct("Document", 1)?;
+        serializer.serialize_field("title_citation_display", &self.title.first())?;
+        serializer.serialize_field("title_display", &self.title.first())?;
+        serializer.serialize_field("format", "Senior Thesis")?;
+        serializer.end()
+    }
+}
+
+fn json_document(path: String) -> String {
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let metadata: Metadata = serde_xml_rs::SerdeXml::new()
+        .namespace("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc/")
+        .namespace("dc", "http://purl.org/dc/elements/1.1/")
+        .from_str(&data)
+        .expect("Unable to parse XML");
+    serde_json::to_string(&metadata.thesis).unwrap()
+}
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("BibdataRs")?;
-    module.define_singleton_method("hello", function!(hello, 0))?;
+    let submodule = module.define_module("Theses")?;
+    submodule.define_singleton_method("json_document", function!(json_document, 1))?;
     Ok(())
 }
