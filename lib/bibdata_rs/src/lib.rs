1b2f9e6b022efa176a7a2baf52ed4fab3fe27522
Jane Sandberg <sandbergja@users.noreply.github.com>
2025-05-07 13:54:43 -0700

    Move theses into its own module (#2705)
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

--- lib/bibdata_rs/src/lib.rs
+++ lib/bibdata_rs/src/lib.rs
@@ -2,17 +2,7 @@ use magnus::{function, prelude::*, Error, Ruby};
 use serde::{ser::SerializeStruct, Deserialize, Serialize, Serializer};
 use std::fs;
 
-#[derive(Deserialize)]
-struct Metadata {
-    #[serde(rename = "oai_dc:dc")]
-    thesis: Thesis,
-}
-
-#[derive(Debug, Deserialize)]
-struct Thesis {
-    #[serde(rename = "dc:title")]
-    title: Vec<String>,
-}
+mod theses;
 
 #[derive(Deserialize, Debug)]
 pub struct Response {
@@ -89,40 +79,21 @@ impl Attributes {
     }
 }
 
-impl Serialize for Thesis {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut serializer = serializer.serialize_struct("Document", 1)?;
-        serializer.serialize_field("title_citation_display", &self.title.first())?;
-        serializer.serialize_field("title_display", &self.title.first())?;
-        serializer.serialize_field("format", "Senior Thesis")?;
-        serializer.end()
-    }
-}
+
 fn json_ephemera_document(path: String) -> String {
     let data = fs::read_to_string(path).expect("Unable to read file");
     let metadata: Attributes = serde_json::from_str(&data).expect("Unable to parse JSON");
     serde_json::to_string(&metadata).unwrap()
 }
 
-fn json_theses_document(path: String) -> String {
-    let data = fs::read_to_string(path).expect("Unable to read file");
-    let metadata: Metadata = serde_xml_rs::SerdeXml::new()
-        .namespace("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc/")
-        .namespace("dc", "http://purl.org/dc/elements/1.1/")
-        .from_str(&data)
-        .expect("Unable to parse XML");
-    serde_json::to_string(&metadata.thesis).unwrap()
-}
+
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("BibdataRs")?;
     let submodule = module.define_module("Theses")?;
     let submodule_ephemera = submodule.define_module("Ephemera")?;
-    submodule.define_singleton_method("json_document", function!(json_theses_document, 1))?;
+    submodule.define_singleton_method("json_document", function!(theses::json_document, 1))?;
     submodule_ephemera
         .define_singleton_method("json_document", function!(json_ephemera_document, 1))?;
     Ok(())
