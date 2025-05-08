28150a0b5f92626aed21f09a15bf146b28323010
Christina Chortaria <actspatial@gmail.com>
2025-05-08 15:07:58 -0400

    Include mockito in ephemera_folders test
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>
    Co-authored-by: Ryan Laddusaw <rladdusaw@users.noreply.github.com>

--- lib/bibdata_rs/src/ephemera/ephemera_folder.rs
+++ lib/bibdata_rs/src/ephemera/ephemera_folder.rs
@@ -1,8 +1,5 @@
 use std::path::PathBuf;
-
-use mockito::mock;
 use serde::Deserialize;
-
 use super::CatalogClient;
 
 #[derive(Deserialize, Debug)]
@@ -32,27 +29,38 @@ pub async fn read_ephemera_folders() -> Result<Vec<String>, Box<dyn std::error::
         .collect();
     Ok(ids)
 }
+#[cfg(test)]
+mod tests {
+ 
+    use mockito::mock;
+    use std::path::PathBuf;
 
-#[tokio::test]
-async fn test_read_ephemera_folders() {
-    let mock_url = mockito::server_url();
-    std::env::set_var("FIGGY_BORN_DIGITAL_EPHEMERA_URL", &mock_url);
+    use crate::ephemera::ephemera_folder::read_ephemera_folders;
 
-    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    d.push("../../spec/fixtures/files/ephemera/ephemera_folders.json");
-
-    let mock = mock("GET", "/")
-        .with_status(200)
-        .with_header("content-type", "application/json")
-        .with_body_from_file(d.to_string_lossy().to_string())
-        .create();
-
-    let result = read_ephemera_folders().await.unwrap();
-    assert!(!result.is_empty());
-    assert!(result.contains(
-        &"https://figgy-staging.princeton.edu/catalog/af4a941d-96a4-463e-9043-cfa512e5eddd"
-            .to_string()
-    ));
-
-    mock.assert();
+    #[tokio::test]
+    async fn test_read_ephemera_folders() {
+        let mock_url = mockito::server_url();
+        std::env::set_var("FIGGY_BORN_DIGITAL_EPHEMERA_URL", &mock_url);
+    
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("../../spec/fixtures/files/ephemera/ephemera_folders.json");
+    
+        let mock = mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body_from_file(d.to_string_lossy().to_string())
+            .create();
+    
+        let result = read_ephemera_folders().await.unwrap();
+        assert!(!result.is_empty());
+        assert!(result.contains(
+            &"https://figgy-staging.princeton.edu/catalog/af4a941d-96a4-463e-9043-cfa512e5eddd"
+                .to_string()
+        ));
+    
+        mock.assert();
+    }
 }
+
+
+
