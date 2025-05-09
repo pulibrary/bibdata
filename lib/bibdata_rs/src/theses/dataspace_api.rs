10ff3ef012702f77e847a904a3fc1c25214b7be9
Jane Sandberg <js7389@princeton.edu>
2025-05-09 13:16:35 -0700

    rustify more tests, remove more unused ruby

--- lib/bibdata_rs/src/theses/dataspace_api.rs
+++ lib/bibdata_rs/src/theses/dataspace_api.rs
@@ -206,12 +206,15 @@ fn title_sort(titles: &Option<Vec<String>>) -> Option<String> {
             Some(
                 first
                     .to_lowercase()
+                    .chars()
+                    .filter(|c| !c.is_ascii_punctuation())
+                    .collect::<String>()
                     .trim_start_matches("a ")
                     .trim_start_matches("an ")
                     .trim_start_matches("the ")
                     .chars()
-                    .filter(|c| !c.is_whitespace() && !c.is_ascii_punctuation())
-                    .collect::<String>(),
+                    .filter(|c| !c.is_whitespace())
+                    .collect::<String>()
             )
         }
         None => None,
@@ -226,6 +229,15 @@ pub fn ruby_json_to_solr_json(ruby: String) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    static EXAMPLE_DOC: &str = r#"{"id":"dsp01b2773v788","dc.description.abstract":["Summary"],"dc.contributor":["Wolff, Tamsen"],
+    "dc.contributor.advisor":["Sandberg, Robert"],"dc.contributor.author":["Clark, Hillary"],"dc.date.accessioned":["2013-07-11T14:31:58Z"],"dc.date.available":["2013-07-11T14:31:58Z"],
+    "dc.date.created":["2013-04-02"],"dc.date.issued":["2013-07-11"],
+    "dc.format.extent":["102 pages"],"dc.language.iso":["en_US"],"dc.title":["Dysfunction: A Play in One Act"],
+    "dc.type":["Princeton University Senior Theses"],"pu.date.classyear":["2014"],
+    "pu.department":["Princeton University. Department of English","Princeton University. Program in Theater"],"pu.pdf.coverpage":["SeniorThesisCoverPage"],
+    "dc.rights.accessRights":["Walk-in Access..."]}"#;
+
     #[test]
     fn it_can_parse_json() {
         let metadata: Metadata = serde_json::from_str(
@@ -239,13 +251,7 @@ mod tests {
 
     #[test]
     fn it_can_convert_into_solr_document() {
-        let metadata: Metadata = serde_json::from_str(r#"{"id":"dsp01b2773v788","dc.description.abstract":["Summary"],"dc.contributor":["Wolff, Tamsen"],
-        "dc.contributor.advisor":["Sandberg, Robert"],"dc.contributor.author":["Clark, Hillary"],"dc.date.accessioned":["2013-07-11T14:31:58Z"],"dc.date.available":["2013-07-11T14:31:58Z"],
-        "dc.date.created":["2013-04-02"],"dc.date.issued":["2013-07-11"],"dc.identifier.uri":["http://arks.princeton.edu/ark:/88435/dsp01b2773v788"],
-        "dc.format.extent":["102 pages"],"dc.language.iso":["en_US"],"dc.title":["Dysfunction: A Play in One Act"],
-        "dc.type":["Princeton University Senior Theses"],"pu.date.classyear":["2014"],
-        "pu.department":["Princeton University. Department of English","Princeton University. Program in Theater"],"pu.pdf.coverpage":["SeniorThesisCoverPage"],
-        "dc.rights.accessRights":["Walk-in Access..."]}"#).unwrap();
+        let metadata: Metadata = serde_json::from_str(EXAMPLE_DOC).unwrap();
         let solr = solr::SolrDocument::from(metadata);
 
         assert_eq!(solr.id, "dsp01b2773v788");
@@ -268,7 +274,72 @@ mod tests {
     fn it_can_create_sortable_version_of_title() {
         assert_eq!(
             title_sort(&Some(vec!["\"Some quote\" : Blah blah".to_owned()])).unwrap(),
-            "somequoteblahblah"
+            "somequoteblahblah",
+            "it should strip punctuation"
         );
+        assert_eq!(
+            title_sort(&Some(vec!["A title : blah blah".to_owned()])).unwrap(),
+            "titleblahblah",
+            "it should strip articles"
+        );
+        assert_eq!(
+            title_sort(&Some(vec!["\"A quote\" : Blah blah".to_owned()])).unwrap(),
+            "quoteblahblah",
+            "it should strip punctuation and articles"
+        );
+        assert_eq!(
+            title_sort(&Some(vec!["thesis".to_owned()])).unwrap(),
+            "thesis",
+            "it should leave words that start with articles alone"
+        );
+    }
+
+    #[test]
+    fn ark_hash_gets_the_ark_with_fulltext_link_display_when_restrictions() {
+        let metadata: Metadata = serde_json::from_str(r#"{"id":"dsp01b2773v788","dc.description.abstract":["Summary"],"dc.contributor":["Wolff, Tamsen"],
+        "dc.contributor.advisor":["Sandberg, Robert"],"dc.contributor.author":["Clark, Hillary"],"dc.date.accessioned":["2013-07-11T14:31:58Z"],"dc.date.available":["2013-07-11T14:31:58Z"],
+        "dc.date.created":["2013-04-02"],"dc.date.issued":["2013-07-11"],"dc.identifier.uri":["http://arks.princeton.edu/ark:/88435/dsp01b2773v788"],
+        "dc.format.extent":["102 pages"],"dc.language.iso":["en_US"],"dc.title":["Dysfunction: A Play in One Act"],
+        "dc.type":["Princeton University Senior Theses"],"pu.date.classyear":["2014"],
+        "pu.department":["Princeton University. Department of English","Princeton University. Program in Theater"],"pu.pdf.coverpage":["SeniorThesisCoverPage"],
+        "dc.rights.accessRights":["Walk-in Access..."]}"#).unwrap();
+        assert_eq!(metadata.ark_hash().unwrap(), r#"{"http://arks.princeton.edu/ark:/88435/dsp01b2773v788":["DataSpace","Full text"]}"#);
+    }
+
+    #[test]
+    fn ark_hash_gets_the_ark_with_fulltext_link_display_when_no_restrictions() {
+        let metadata: Metadata = serde_json::from_str(r#"{"id":"dsp01b2773v788","dc.description.abstract":["Summary"],"dc.contributor":["Wolff, Tamsen"],
+        "dc.contributor.advisor":["Sandberg, Robert"],"dc.contributor.author":["Clark, Hillary"],"dc.date.accessioned":["2013-07-11T14:31:58Z"],"dc.date.available":["2013-07-11T14:31:58Z"],
+        "dc.date.created":["2013-04-02"],"dc.date.issued":["2013-07-11"],"dc.identifier.uri":["http://arks.princeton.edu/ark:/88435/dsp01b2773v788"],
+        "dc.format.extent":["102 pages"],"dc.language.iso":["en_US"],"dc.title":["Dysfunction: A Play in One Act"],
+        "dc.type":["Princeton University Senior Theses"],"pu.date.classyear":["2014"],
+        "pu.department":["Princeton University. Department of English","Princeton University. Program in Theater"],"pu.pdf.coverpage":["SeniorThesisCoverPage"]}"#).unwrap();
+        assert_eq!(metadata.ark_hash().unwrap(), r#"{"http://arks.princeton.edu/ark:/88435/dsp01b2773v788":["DataSpace","Full text"]}"#);
+    }
+
+    #[test]
+    fn ark_hash_returns_none_when_no_url() {
+        let metadata: Metadata = serde_json::from_str(r#"{"id":"dsp01b2773v788","dc.description.abstract":["Summary"],"dc.contributor":["Wolff, Tamsen"],
+        "dc.contributor.advisor":["Sandberg, Robert"],"dc.contributor.author":["Clark, Hillary"],"dc.date.accessioned":["2013-07-11T14:31:58Z"],"dc.date.available":["2013-07-11T14:31:58Z"],
+        "dc.date.created":["2013-04-02"],"dc.date.issued":["2013-07-11"],
+        "dc.format.extent":["102 pages"],"dc.language.iso":["en_US"],"dc.title":["Dysfunction: A Play in One Act"],
+        "dc.type":["Princeton University Senior Theses"],"pu.date.classyear":["2014"],
+        "pu.department":["Princeton University. Department of English","Princeton University. Program in Theater"],"pu.pdf.coverpage":["SeniorThesisCoverPage"],
+        "dc.rights.accessRights":["Walk-in Access..."]}"#).unwrap();
+        assert_eq!(metadata.ark_hash(), None);
+    }
+
+    #[test]
+    fn on_site_only() {
+        assert!(serde_json::from_str::<Metadata>(r#"{"pu.embargo.terms":["2100-01-01"]}"#).unwrap().on_site_only(), "doc with embargo terms field should return true");
+        assert!(serde_json::from_str::<Metadata>(r#"{"pu.embargo.lift":["2100-01-01"]}"#).unwrap().on_site_only(), "doc with embargo lift field should return true");
+        assert!(serde_json::from_str::<Metadata>(r#"{"pu.embargo.lift":["2000-01-01"], "pu.mudd.walkin":["yes"], "pu.date.classyear":["2012-01-01T00:00:00Z"]}"#).unwrap().on_site_only(), "with a specified accession date prior to 2013, it should return true");
+
+        assert!(!serde_json::from_str::<Metadata>(r#"{"pu.location":["physical location"]}"#).unwrap().on_site_only(), "doc with location field should return false");
+        assert!(!serde_json::from_str::<Metadata>(r#"{"pu.embargo.lift":["2000-01-01"]}"#).unwrap().on_site_only(), "doc with expired embargo lift field should return false");
+        assert!(!serde_json::from_str::<Metadata>(r#"{"pu.embargo.lift":["2000-01-01"], "pu.mudd.walkin":["yes"]}"#).unwrap().on_site_only(), "without a specified accession date, it should return false");
+        assert!(!serde_json::from_str::<Metadata>(r#"{"pu.embargo.lift":["2000-01-01"], "pu.mudd.walkin":["yes"], "pu.date.classyear":["2013-01-01T00:00:00Z"]}"#).unwrap().on_site_only(), "with a specified accession date in 2013, it should return false");
+        assert!(!serde_json::from_str::<Metadata>(r#"{}"#).unwrap().on_site_only(), "doc with no access-related fields should return false");
+        assert!(!serde_json::from_str::<Metadata>(EXAMPLE_DOC).unwrap().on_site_only());
     }
 }
