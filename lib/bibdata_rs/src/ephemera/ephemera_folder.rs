540eeca3c9e18584fd56527bfe1647a3d9ebad94
Christina Chortaria <actspatial@gmail.com>
2025-05-13 16:37:41 -0400

    Update read_ephemera_folders to read ids and not links with ids pass str url in ephemera_folders_iterator Remove std::env::var as it does not set the var. Improve this in future work.\ pass str url in chunk_read_id Update test_ephemera_folders_iterator to use a regular expression to mock figgy ids
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- lib/bibdata_rs/src/ephemera/ephemera_folder.rs
+++ lib/bibdata_rs/src/ephemera/ephemera_folder.rs
@@ -20,37 +20,37 @@ pub struct Links {
     #[serde(rename = "self")]
     url: String,
 }
-pub async fn read_ephemera_folders(url: impl Into<String>) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+pub async fn read_ephemera_folders(
+    url: impl Into<String>,
+) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let client = CatalogClient::new(url.into());
     let response = client.get_folder_data().await?;
 
-    let ids: Vec<String> = response
-        .data
-        .into_iter()
-        .map(|item| item.links.url)
-        .collect();
+    let ids: Vec<String> = response.data.into_iter().map(|item| item.id).collect();
     Ok(ids)
 }
 
 pub async fn ephemera_folders_iterator(
+    url: &str,
 ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let data: Vec<String> = read_ephemera_folders(std::env::var("FIGGY_PRODUCTION").unwrap_or("https://figgy.princeton.edu".to_string())).await?;
+    let data: Vec<String> = read_ephemera_folders(url).await?;
     let chunk_size = 1000;
     let mut result: Vec<String> = Vec::new();
     for chunk in data.chunks(chunk_size) {
         let chunk_vec: Vec<String> = chunk.to_vec().clone();
-        let responses = chunk_read_id(chunk_vec).await?;
+        let responses = chunk_read_id(chunk_vec, &url).await?;
         result.push(responses);
     }
     Ok(result)
 }
 
 pub async fn chunk_read_id(
-    ids: Vec<String>
+    ids: Vec<String>,
+    url: &str,
 ) -> Result<String, Box<dyn std::error::Error>> {
     let mut responses = Vec::new();
     for id in ids {
-        let client = CatalogClient::default();
+        let client = CatalogClient::new(url.to_owned());
         let response = client.get_item_data(&id).await?;
         responses.push(response);
     }
@@ -73,7 +73,6 @@ mod tests {
     async fn test_read_ephemera_folders() {
         preserving_envvar_async("FIGGY_BORN_DIGITAL_EPHEMERA_URL", || async {
             let mut server = mockito::Server::new_async().await;
-            // std::env::set_var("FIGGY_BORN_DIGITAL_EPHEMERA_URL", &server.url());
 
             let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
             d.push("../../spec/fixtures/files/ephemera/ephemera_folders.json");
@@ -87,7 +86,6 @@ mod tests {
                 .create_async()
                 .await;
 
-            println!("SERVER URL IS {}", server.url());
             let result = read_ephemera_folders(server.url()).await.unwrap();
             assert!(!result.is_empty());
             assert!(result.contains(
@@ -102,38 +100,40 @@ mod tests {
 
     #[tokio::test]
     async fn test_ephemera_folders_iterator() {
-        preserving_envvar_async("FIGGY_STAGING_URL", || async {
-            let mut server = mockito::Server::new_async().await;
+        let mut server = mockito::Server::new_async().await;
 
-            let mock1 = server
+        let mock1 = server
                 .mock("GET", "/catalog.json?f%5Bephemera_project_ssim%5D%5B%5D=Born+Digital+Monographs%2C+Serials%2C+%26+Series+Reports&f%5Bhuman_readable_type_ssim%5D%5B%5D=Ephemera+Folder&f%5Bstate_ssim%5D%5B%5D=complete&per_page=100&q=")
                 .with_status(200)
                 .with_header("content-type", "application/json")
                 .with_body_from_file("../../spec/fixtures/files/ephemera/ephemera_folders.json")
                 .create();
 
-            let mock2 = server
-                .mock("GET", "/catalog/af4a941d-96a4-463e-9043-cfa511e5eddd")
-                .with_status(200)
-                .with_header("content-type", "application/json")
-                .with_body_from_file("../../spec/fixtures/files/ephemera/ephemera1.json")
-                .create();
-            let data = read_ephemera_folders(server.url()).await.unwrap();
-            let chunk_size = 3;
-            let mut result: Vec<String> = Vec::new();
-            for chunk in data.chunks(chunk_size) {
-                let chunk_vec: Vec<String> = chunk.to_vec();
-                let responses = chunk_read_id(chunk_vec).await.unwrap();
-                result.push(responses);
-            }
-            // let result = ephemera_folders_iterator().await.unwrap();
-            assert!(!result.is_empty());
-            // Assuming your ephemera_folders.json has 1 item
-            assert_eq!(result.len(), 1); // Total chunks should be 1
+        let mock2 = server
+            .mock(
+                "GET",
+                mockito::Matcher::Regex(
+                    r"^/catalog/[0-9]{8}-[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{12}$".to_string(),
+                ),
+            )
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body_from_file("../../spec/fixtures/files/ephemera/ephemera1.json")
+            .create();
+        let data = read_ephemera_folders(server.url()).await.unwrap();
+        let chunk_size = 3;
+        let mut result: Vec<String> = Vec::new();
+        for chunk in data.chunks(chunk_size) {
+            let chunk_vec: Vec<String> = chunk.to_vec();
+            let responses = chunk_read_id(chunk_vec, &server.url()).await.unwrap();
+            result.push(responses);
+        }
+        // let result = ephemera_folders_iterator().await.unwrap();
+        assert!(!result.is_empty());
+        // Assuming your ephemera_folders.json has 1 item
+        assert_eq!(result.len(), 1); // Total chunks should be 1
 
-            mock1.assert();
-            mock2.assert();
-        })
-        .await;
+        mock1.assert();
+        mock2.assert();
     }
 }
