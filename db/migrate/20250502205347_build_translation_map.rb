dd8898809357d7676f057feb622440ea22d8c3d9
Max Kadel <mkadel@princeton.edu>
2025-05-19 11:23:23 -0400

    Rescue from error for development

--- db/migrate/20250502205347_build_translation_map.rb
+++ db/migrate/20250502205347_build_translation_map.rb
@@ -1,5 +1,7 @@
 class BuildTranslationMap < ActiveRecord::Migration[7.2]
   def change
     Rake::Task['figgy_mms_ids:build_translation_map'].invoke
+  rescue MmsRecordsReport::AuthenticationError
+    puts("Cannot authenticate to build figgy_mms_ids translation map, skipping.")
   end
 end
