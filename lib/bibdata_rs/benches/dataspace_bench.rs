931edcafd510457dd9c3eb3e49bec6e675c1b147
Jane Sandberg <js7389@princeton.edu>
2025-06-05 13:42:14 -0700

    Add a benchmark for deserializing dspace json and converting it to solr json

--- lib/bibdata_rs/benches/dataspace_bench.rs
+++ lib/bibdata_rs/benches/dataspace_bench.rs
@@ -1,9 +1,14 @@
-use bibdata_rs::theses::dataspace::document;
+use std::{fs::File, io::BufReader};
+
+use bibdata_rs::{
+    solr::SolrDocument,
+    theses::dataspace::document::{self, DataspaceDocument},
+};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn title_normalize_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("title_normalize");
-    group.bench_function("map", |b| {
+    group.bench_function("title_search_versions", |b| {
         b.iter(|| {
             let document = document::DataspaceDocument::builder()
                 .with_title("2D \\(^{1}\\)H-\\(^{14}\\)N HSQC inverse-detection experiments")
@@ -15,5 +20,29 @@ fn title_normalize_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(valid_benches, title_normalize_benchmark);
+fn dataspace_to_solr_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("solr_document_from_dataspace_document");
+    group.bench_function("from", |b| {
+        b.iter(|| {
+            let fixture =
+                File::open("../../spec/fixtures/files/theses/dsp01b2773v788.json").unwrap();
+            let reader = BufReader::new(fixture);
+            let documents: Vec<DataspaceDocument> = serde_json::from_reader(reader).unwrap();
+            let solr_documents: Vec<SolrDocument> =
+                documents.iter().map(|d| SolrDocument::from(d)).collect();
+            assert_eq!(
+                solr_documents[0].title_citation_display,
+                Some("Dysfunction: A Play in One Act".to_string())
+            );
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(
+    valid_benches,
+    title_normalize_benchmark,
+    dataspace_to_solr_benchmark
+);
 criterion_main!(valid_benches);
