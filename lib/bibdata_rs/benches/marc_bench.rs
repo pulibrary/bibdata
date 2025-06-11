1f9d596e2234380898818c7cbf8fb35e81023693
Jane Sandberg <js7389@princeton.edu>
2025-06-11 14:33:19 -0400

    [#2773] Use lcgft to determine whether or not a resource is a primary source
    This supplements the existing lcsh-based logic.
    
    Closes #2773

---
+++ lib/bibdata_rs/benches/marc_bench.rs
@@ -0,0 +1,19 @@
+use bibdata_rs::marc::genre::genres;
+use criterion::{criterion_group, criterion_main, Criterion};
+use marctk::Record;
+
+fn genre_facet_benchmark(c: &mut Criterion) {
+    c.bench_function("genres", |b| {
+        b.iter(|| {
+            let record = Record::from_xml_file("../../spec/fixtures/99100026953506421.mrx")
+                .unwrap()
+                .next()
+                .unwrap()
+                .unwrap();
+            assert_eq!(genres(&record), vec!["Periodicals", "Facsimiles"]);
+        })
+    });
+}
+
+criterion_group!(benches, genre_facet_benchmark);
+criterion_main!(benches);
