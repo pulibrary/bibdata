e4d4e847a0abbb9e32b1df1ee365572607ff3c50
Jane Sandberg <js7389@princeton.edu>
2025-06-11 12:13:30 -0400

    Add rust tests for strip_non_numeric

--- lib/bibdata_rs/src/marc/string_normalize.rs
+++ lib/bibdata_rs/src/marc/string_normalize.rs
@@ -62,4 +62,15 @@ mod tests {
         assert_eq!("Le réve", trim_punctuation("Le réve."));
         assert_eq!("Bill Dueber, Jr.", trim_punctuation("Bill Dueber, Jr."));
     }
+
+    #[test]
+    fn it_strips_non_numeric() {
+        assert_eq!(strip_non_numeric("abc"), "");
+        assert_eq!(strip_non_numeric("123"), "123");
+        assert_eq!(strip_non_numeric("0123"), "123");
+        assert_eq!(strip_non_numeric("abc0123"), "123");
+        assert_eq!(strip_non_numeric("0abc123"), "123");
+        assert_eq!(strip_non_numeric("000abc123"), "123");
+        assert_eq!(strip_non_numeric("000abc0123"), "123");
+    }
 }
