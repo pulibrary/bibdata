cec6edd6a8eb8dbb72abdc3b7eb62b1f67ff32d8
Jane Sandberg <js7389@princeton.edu>
2026-04-15 08:41:16 -0700

    Upgrade marctk to 0.7.0
    It has not yet been released, but has some performance improvements.
    It also has a new repo: https://github.com/kcls/marctk
    
    So this helps to test that the new repo is behaving as expected
    as well (see https://github.com/kcls/evergreen-universe-rs/issues/32)

--- lib/bibdata_rs/src/marc/identifier/isbn.rs
+++ lib/bibdata_rs/src/marc/identifier/isbn.rs
@@ -1,7 +1,7 @@
 use library_stdnums::{isbn::ISBN, traits::Normalize};
 use marctk::Record;
 
-pub fn normalized_isbns_for_all_versions(record: &Record) -> impl Iterator<Item = String> + use<> {
+pub fn normalized_isbns_for_all_versions(record: &Record) -> impl Iterator<Item = String> + use<'_> {
     record
         .extract_values("020az:776z")
         .into_iter()
