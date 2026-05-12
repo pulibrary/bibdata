ed7a18a3a04704a3c2e6f17cff3fa92212e24f45
Christina Chortaria <actspatial@gmail.com>
2026-05-12 10:02:41 -0400

    This part was moved in pull request https://github.com/pulibrary/bibdata/pull/3281/changes/9c8450db0096a1377ab689d170d25d58797561fa
    In this commit we keep only the require 'sidekiq/api'
    
    refactor the helper test method run_all_callbacks
    
    zadd - a redis command that adds a member to a sorted set with a numeric score.
    Redis sorted sets store members in order by their score.
    
    Pro 7 — score based when Batch.new was called creation time + expiry:
    m.zadd("batches", @created_f + expiry, bid)
    
    Pro 8 — score based when the jobs block finishes, now + expiry:
    m.zadd("batches", (Time.now + expiry).to_f, bid)
    
    This difference changes to_a ordering only in tests because of the Sidekiq::Testing.inline!: with inline! the jobs calls perform_async which runs the job synchronously. this job calls index_next_dump
    to_a order in pro7 is [parent,child] <- ZRANGE
    to_a order in pro8 is [child, parent] <- ZRANGE
    
    In pro7 the test assumes that to_a.last is the child because of the ZRANGE.
    In pro8 the parent will not finish until everything inside the parent block has run. because of the ZRANGE if we use to_a.last the parent will wait for everyhting inside to run then it will run last and the reindex! will call .wipe! and delete the records that were indexed from the child. So in this case to_a.last will call the parent.

--- spec/models/index_manager_spec.rb
+++ spec/models/index_manager_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'sidekiq/api'
 
 RSpec.describe IndexManager, :indexing, :sidekiq, type: :model do
   let(:solr_url) { ENV.fetch('SOLR_URL', nil) || "http://#{ENV.fetch('lando_bibdata_test_solr_conn_host', nil)}:#{ENV.fetch('lando_bibdata_test_solr_conn_port', nil)}/solr/bibdata-core-test" }
