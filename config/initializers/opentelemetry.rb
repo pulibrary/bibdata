8bfb9005fafaf1033c2ef6f4e2fad46ff0354559
Christina Chortaria <actspatial@gmail.com>
2026-05-13 22:05:24 -0400

    Revert "Signoz two electric"

--- config/initializers/opentelemetry.rb
+++
@@ -1,8 +0,0 @@
-require 'opentelemetry/sdk'
-require 'opentelemetry/exporter/otlp'
-require 'opentelemetry/instrumentation/all'
-
-OpenTelemetry::SDK.configure do |c|
-  c.use_all  # Enables all available instrumentations
-end
-
