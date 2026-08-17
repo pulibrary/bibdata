3b0d76f100df888a515e5eb1c5e0bd3bc08cfed9
Christina Chortaria <actspatial@gmail.com>
2026-08-17 10:58:22 -0400

    use a compressed raw marcxml

--- lib/bibdata_rs/src/marc/marcxml_binary.rs
+++ lib/bibdata_rs/src/marc/marcxml_binary.rs
@@ -1,12 +1,24 @@
-use base64::{Engine as _, engine::general_purpose::STANDARD};
+// use base64::{Engine as _, engine::general_purpose::STANDARD};
+use flate2::{Compression, write::GzEncoder};
 use marctk::Record;
+use std::io::Write;
 
-pub fn marc21_binary_b64(record: &Record) -> Result<String, Box<dyn std::error::Error>> {
-    let bytes = record.to_binary()?; // raw MARC21 (ISO2709) bytes
-    Ok(STANDARD.encode(bytes)) // JSON-safe base64
-}
+// pub fn marc21_binary_b64(record: &Record) -> Result<String, Box<dyn std::error::Error>> {
+//     let bytes = record.to_binary()?; // raw MARC21 (ISO2709) bytes
+//     Ok(STANDARD.encode(bytes)) // JSON-safe base64
+// }
 
 // pub fn marc21_binary_raw(record: &Record) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 //     let bytes = record.to_binary()?; // raw MARC21 (ISO2709) bytes
 //     Ok(bytes)
 // }
+
+pub fn marc21_binary_gz_raw_bytes(record: &Record) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let bytes = record.to_binary()?; // raw MARC21 (ISO2709) bytes
+
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(&bytes)?; // gzip-compress the bytes
+    let compressed = encoder.finish()?; // return compressed bytes
+
+    Ok(compressed)
+}
