7c370f77d0eb726d9047efe2474ea0ac0c52433c
Christina Chortaria <actspatial@gmail.com>
2026-08-17 14:40:33 -0400

    add marcxml_bi in traject_config.rb

--- lib/bibdata_rs/src/marc/ruby_bindings.rs
+++ lib/bibdata_rs/src/marc/ruby_bindings.rs
@@ -14,8 +14,6 @@ use crate::marc::fixed_field::dates::BeginDate;
 use crate::marc::holdings::partner::partner_holdings;
 use crate::marc::identifier::identifiers_of_all_versions;
 use crate::marc::identifier::map_024_indicators_to_labels;
-// use crate::marc::marcxml_binary::marc21_binary_b64;
-use crate::marc::marcxml_binary::marc21_binary_gz_raw_bytes;
 use crate::marc::marcxml_compressor::marcxml_compressed;
 use crate::marc::note::access_notes;
 use crate::marc::note::action_note::action_notes;
@@ -302,15 +300,9 @@ fn solr_fields(ruby: &Ruby, record: magnus::RObject) -> Result<RHash, magnus::Er
         extract_marc!("5443abcden")(&record),
     )?;
     hash.aset("marcxml", marcxml_compressed(&record))?;
+    
+    hash.aset("marcxml_bi", marcxml_compressed(&record))?;
 
-    let marcxml_bi = marc21_binary_gz_raw_bytes(&record)
-        .map_err(|e| magnus::Error::new(ruby.exception_arg_error(), e.to_string()))?;
-    // let marcxml_bi = marc21_binary_b64(&record)
-    //     .map_err(|e| magnus::Error::new(ruby.exception_arg_error(), e.to_string()))?;
-
-    hash.aset("marcxml_bi", marcxml_bi)?;
-
-    // hash.aset("marcxml_bi", marc21_binary_b64(&record)?)?;
     hash.aset("methodology_notes_display", extract_marc!("567a")(&record))?;
     hash.aset(
         "non_latin_non_cjk_all_index",
