8a6c6f31f31f1958729b13786bc89c0175b60c56
Ryan Laddusaw <rladdusaw@princeton.edu>
2026-08-31 16:17:33 -0400

    Add some tests and use enums instead of strings

--- lib/bibdata_rs/src/marc/utils/display_format.rs
+++ lib/bibdata_rs/src/marc/utils/display_format.rs
@@ -5,23 +5,43 @@ use crate::solr::format_facet::Abbreviation;
 // this is intended for displaying compact badges inline with search results
 pub fn display_format(formats: Vec<String>) -> String {
     match formats {
-        _ if formats.contains(&"Microform".to_string()) => Microform.abbreviation(),
-        _ if formats.contains(&"Manuscript".to_string()) => Manuscript.abbreviation(),
-        _ if formats.contains(&"Data file".to_string()) => DataFile.abbreviation(),
-        _ if formats.contains(&"Musical score".to_string()) => MusicalScore.abbreviation(),
-        _ if formats.contains(&"Senior thesis".to_string()) => SeniorThesis.abbreviation(),
-        _ if formats.contains(&"Archival item".to_string()) => ArchivalItem.abbreviation(),
-        _ if formats.contains(&"Video/Projected medium".to_string()) => {
+        _ if formats.contains(&Microform.to_string()) => Microform.abbreviation(),
+        _ if formats.contains(&Manuscript.to_string()) => Manuscript.abbreviation(),
+        _ if formats.contains(&DataFile.to_string()) => DataFile.abbreviation(),
+        _ if formats.contains(&MusicalScore.to_string()) => MusicalScore.abbreviation(),
+        _ if formats.contains(&SeniorThesis.to_string()) => SeniorThesis.abbreviation(),
+        _ if formats.contains(&ArchivalItem.to_string()) => ArchivalItem.abbreviation(),
+        _ if formats.contains(&VideoProjectedMedium.to_string()) => {
             VideoProjectedMedium.abbreviation()
         }
-        _ if formats.contains(&"Visual material".to_string()) => VisualMaterial.abbreviation(),
-        _ if formats.contains(&"Map".to_string()) => Map.abbreviation(),
-        _ if formats.contains(&"Report".to_string()) => Report.abbreviation(),
-        _ if formats.contains(&"Coin".to_string()) => Coin.abbreviation(),
-        _ if formats.contains(&"Databases".to_string()) => Databases.abbreviation(),
-        _ if formats.contains(&"Audio".to_string()) => Audio.abbreviation(),
-        _ if formats.contains(&"Book".to_string()) => Book.abbreviation(),
-        _ if formats.contains(&"Journal".to_string()) => Journal.abbreviation(),
+        _ if formats.contains(&VisualMaterial.to_string()) => VisualMaterial.abbreviation(),
+        _ if formats.contains(&Map.to_string()) => Map.abbreviation(),
+        _ if formats.contains(&Report.to_string()) => Report.abbreviation(),
+        _ if formats.contains(&Coin.to_string()) => Coin.abbreviation(),
+        _ if formats.contains(&Databases.to_string()) => Databases.abbreviation(),
+        _ if formats.contains(&Audio.to_string()) => Audio.abbreviation(),
+        _ if formats.contains(&Book.to_string()) => Book.abbreviation(),
+        _ if formats.contains(&Journal.to_string()) => Journal.abbreviation(),
         _ => "".to_string(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+#[test]
+    fn it_prioritizes_correctly() {
+        assert_eq!(
+            display_format(vec!("Microform".to_string(), "Book".to_string())),
+            Microform.abbreviation()
+        );
+        assert_eq!(
+            display_format(vec!("Journal".to_string(), "Book".to_string())),
+            Book.abbreviation()
+        );
+        assert_eq!(
+            display_format(vec!("Musical score".to_string(), "Data file".to_string())),
+            DataFile.abbreviation()
+        );
     }
 }
